a09cbaf9e24f6f0e6feebbba13047374f3353140
Derek Gomez <demagome@ucsc.edu>
2024-11-20 22:54:04 -0800

    COMMIT Binary Search

--- Binary Search/Binary Search.py
+++ Binary Search/Binary Search.py
@@ -1,32 +1,18 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         
-        ans = -1
-        # recursive binary search
-        def BSA(l, r):
-            m = (l + r) // 2
-            # print(str(nums[l:r]))
-            # print(str(m), "->" ,str(nums[m]))
+        l, r = 0, len(nums) - 1
 
-            # base case
-            if l >= r:
-                pass
+        while(l <= r):
+            m = (l+r) // 2
 
-            # compare middle to target
-            elif nums[m] < target:
-                # print("<")
-                BSA(m+1, r)
+            if nums[m] < target:
+                l = m + 1
+
             elif nums[m] > target:
-                # print(">")
-                BSA(l, m)
-            elif nums[m] == target:
-                # print("=")
-                nonlocal ans
-                ans = m
-            # else:
-            #     print("uh oh")
-        
-        BSA(0, len(nums))
+                r = m - 1
 
-        # target is never found
-        return ans
+            elif nums[m] == target:
+                return m
+        
+        return -1
