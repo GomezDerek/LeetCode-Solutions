82360df84ea32040a2d7ff150d6bf9b847e6200d
Derek Gomez <demagome@ucsc.edu>
2024-11-22 00:15:10 -0800

    COMMIT Merge Two Sorted Lists

--- Merge Two Sorted Lists/Merge Two Sorted Lists.py
+++ Merge Two Sorted Lists/Merge Two Sorted Lists.py
@@ -5,34 +5,23 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-
-        # 5 min reading and planning
-        # 13 min trying to figure out how to access attributes in python3, but edge cases were screwing me over >:(
-        # called it quits after 42 min and watched NC explanation
-       
-        nuHead = ListNode()
-        nuTail = nuHead
-
-        # edge case - empty lists
-        if not list2: return list1
-        elif not list1: return list2
+        
+        ans = ListNode(None)
+        traveler = ans
 
         while list1 and list2:
-            
             if list1.val < list2.val:
-                nuTail.next = list1
+                traveler.next = ListNode(list1.val)
                 list1 = list1.next
-
-            else: # list1.val >= list2.val
-                nuTail.next = list2
+            else: # list2.val <= list1.val
+                traveler.next = ListNode(list2.val)
                 list2 = list2.next
+            traveler = traveler.next
 
-            nuTail = nuTail.next
 
-        # handle remainders
         if list1:
-            nuTail.next = list1
+            traveler.next = list1
         elif list2:
-            nuTail.next = list2
+            traveler.next = list2
         
-        return nuHead.next
+        return ans.next
