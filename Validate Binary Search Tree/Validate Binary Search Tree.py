100f6d9e6d3d3ce1edca77d608a02d0351914cb8
Derek Gomez <demagome@ucsc.edu>
2024-12-13 10:44:30 -0800

    COMMIT Validate Binary Search Tree

--- Validate Binary Search Tree/Validate Binary Search Tree.py
+++ Validate Binary Search Tree/Validate Binary Search Tree.py
@@ -15,19 +15,22 @@ class Solution:
 
             # recursive call(s)
             left = dfs(node.left)
+            # print(node.val, "lef", left)
             if left["valid"] is False: return left
             if left["max_val"] is not None and left["max_val"] >= node.val:
                 return {"min_val": None, "max_val": None, "valid": False}
 
             right = dfs(node.right)
+            # print(node.val, "right", right)
             if right["valid"] is False: return right
             if right["min_val"] is not None and right["min_val"] <= node.val:
                 return {"min_val": None, "max_val": None, "valid": False}
 
             # return
+            print(node.val, "\nleft: ", left, "\nright", right)
             return {
                 "min_val": node.val if left["min_val"] is None else left["min_val"],
-                "max_val": node.val if left["max_val"] is None else right["max_val"],
+                "max_val": node.val if right["max_val"] is None else right["max_val"],
                 "valid": True
             }
 
