0e3ca9b0f9cbc51031e002e46e48a35d6968656d
Derek Gomez <demagome@ucsc.edu>
2024-12-19 01:53:06 -0800

    COMMIT Number of Islands

--- Number of Islands/Number of Islands.py
+++ Number of Islands/Number of Islands.py
@@ -1,48 +1,30 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        # strategy
-        # iterate through every coordinate
-        # if land, check if it's an island
+        # NC solution
+        m,n = len(grid), len(grid[0])
 
-        # optimization: BFS + memory
-
-        # new stategy
-        # copy of matrix to track visited
-        # iterate through 2d array
-        # if we find land, BFS the island and add all elements to visited matrix
-
-
-
+        visited = set()
         numIslands = 0
-        m = len(grid)
-        n = len(grid[0])
 
-        stack = []
-        visited = [ [False for _ in range(n)] for _ in range(m)]
+        def bfs(row,col):
+            visited.add((row,col))
+            
+            queue = collections.deque()
+            queue.append((row,col))
+            while queue:
+                x,y = queue.popleft()
+                dxdy = [(0,1), (1,0), (0,-1), (-1,0)]
+                for dX,dY in dxdy:
+                    nX,nY = x+dX, y+dY
+                    if nX in range(m) and nY in range(n) and int(grid[nX][nY]) and (nX,nY) not in visited:
+                        queue.append((nX,nY))
+                        visited.add((nX,nY))
 
+        # double loop to iterate through every element
         for row in range(m):
             for col in range(n):
-                # print(f"{(row,col)} = {grid[row][col]}")
-                if visited[row][col]:
-                    # print("\t alr visited") 
-                    continue # skip bc we've already visited
-                else: 
-                    visited[row][col] = True # mark as visited
-
-                if int(grid[row][col]): # ==1, land found! This is the beginnign of an island
-                    numIslands += 1 
-                    # bfs
-                    stack.append((row,col))
-                    while stack:
-                        currX, currY = stack.pop(0)
-                        # print(f"\t{(currX,currY)}")
-                        visited[currX][currY] = True
-                        
-                        dxdy = [(0,1), (1,0), (0,-1), (-1,0)] # neighbor coordinates
-                        for dX, dY in dxdy:
-                            nX, nY = currX + dX, currY + dY # new coordinates
-                            if 0 <= nX < m and 0 <= nY < n:                   # if in bounds
-                                if not visited[nX][nY] and int(grid[nX][nY]): # and it's unvisited land
-                                    stack.append((nX,nY))                     # add to stack and continue mapping island
+                if int((grid[row][col])) and (row,col) not in visited: # land ho! unvisited land!
+                    numIslands += 1
+                    bfs(row, col)
 
         return numIslands
