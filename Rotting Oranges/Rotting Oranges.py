f1b57701749534ab1141277046f1e8b7e7c2f92e
Derek Gomez <demagome@ucsc.edu>
2024-12-20 23:26:39 -0800

    COMMIT Rotting Oranges

---
+++ Rotting Oranges/Rotting Oranges.py
@@ -0,0 +1,55 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        # strategy
+        # use a data struct to keep track of fresh oranges (unvisited)
+        # fresh orange hash will also keep track of min-minutes
+        # DFS or BFS???
+        # return -1 if traversal finishes, and we still have a fresh orange
+        # otherwise, return the highest min-minutes
+
+        fresh = {}
+
+        def dfs(x,y,minute):
+            # print("DFS activated")
+            # base case(s)
+            # none
+            if grid[x][y] is 1: # if fresh orange
+                if fresh[(x,y)] > minute or fresh[(x,y)] is -1: 
+                    fresh[(x,y)] = minute
+
+            adj_fresh = []
+
+            dxdy = [(0,1),(1,0),(0,-1),(-1,0)]
+            for dx,dy in dxdy:
+                nx,ny = x+dx, y+dy
+                if 0 <= nx < m and 0 <= ny < n and grid[nx][ny] is 1: # if in bounds AND fresh
+                    # adj_fresh.append((nx,ny))
+
+            # recursive call(s)
+            for fx,fy in adj_fresh:
+                dfs(fx,fy,minute+1)
+
+            # return statement
+            # none
+
+        m = len(grid)
+        n = len(grid[0])
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] is 0:   # empty
+                    pass
+                elif grid[i][j] is 1: # fresh
+                    if (i,j) not in fresh.keys():
+                        fresh[(i,j)] = -1
+                elif grid[i][j] is 2: # rotten
+                    dfs(i,j,0)
+                    continue
+                else:
+                    print("!")
+
+        min_minutes = float(inf)
+        for val in fresh.values():
+            if val is -1:
+                return -1
+            else:
+                min_minutes = val if val < min_minutes else min_minutes
