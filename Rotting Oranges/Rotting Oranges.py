de5c3e1320e49f06e3c5567d0696abac0697d97f
Derek Gomez <demagome@ucsc.edu>
2024-12-21 01:23:28 -0800

    COMMIT Rotting Oranges

--- Rotting Oranges/Rotting Oranges.py
+++ Rotting Oranges/Rotting Oranges.py
@@ -1,52 +1,36 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        # strategy 👇
-        # use a hash map to keep track of all fresh oranges and their quickest rotting times
-        # DFS for traversal
-        # return -1 if traversal finishes and we still have an unrotten orange in the fresh hash
-        # otherwise, return the highest rottest time
+        # BFS solution
+        m = len(grid)
+        n = len(grid[0])
 
         fresh = {}
+        q = collections.deque()
 
-        def dfs(x,y,minute):
-            if grid[x][y] is 1: # if fresh orange
-                if (x,y) not in fresh.keys(): # fresh is unvisited
-                        fresh[(x,y)] = minute
-                elif fresh[(x,y)] > minute or fresh[(x,y)] is -1: # we have a quicker rotting time!
-                    fresh[(x,y)] = minute
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] is 2:
+                    q.append((i,j,0))
+                elif grid[i][j] is 1:
+                    fresh[(i,j)] = 11
 
-            # check for fresh oranges to continue DFS in the 4-directionally adjacent coordinates
+        while q:
+            x,y,minute = q.popleft()
+
+            if grid[x][y] is 1:
+                if fresh[(x,y)] > minute: fresh[(x,y)] = minute
+                else: continue # skip the rest of this while loop, this is a suboptimal traversal
+            
             dxdy = [(0,1),(1,0),(0,-1),(-1,0)]
             for dx,dy in dxdy:
                 nx,ny = x+dx, y+dy
-                if 0 <= nx < m and 0 <= ny < n and grid[nx][ny] is 1: # if in bounds AND fresh
-                    # recursive call(s)
-                    if (nx,ny) not in fresh.keys(): # fresh is unvisited
-                        dfs(nx,ny,minute+1)
-                    elif fresh[(nx,ny)] > minute or fresh[(nx,ny)] is -1: # fresh is worth visiting because we have a better rotting time
-                        dfs(nx,ny,minute+1)
+                if nx in range(m) and ny in range(n) and grid[nx][ny] is 1:
+                    q.append((nx,ny,minute+1))
 
-        # double loop through the grid
-        m = len(grid)
-        n = len(grid[0])
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] is 1: # fresh
-                    if (i,j) not in fresh.keys():
-                        fresh[(i,j)] = -1
-                elif grid[i][j] is 2: # rotten
-                    dfs(i,j,0)
+        if not fresh: return 0
 
-                # ignore if no orange in grid cell
-                # elif grid[i][j] is 0 # empty
-
-        # edge case: there was never any fresh oranges to begin with, so 0 minutes until all oranges are rotten
-        if not fresh:
-            return 0
-
-        ans = float(-inf)
-        for val in fresh.values():
-            if val is -1: return -1               # this fresh orange never rots!
-            else: ans = val if val > ans else ans # track the highest rotting time amongst the fresh oranges
-        
-        return ans
+        # invert values to turn min-heap into max-heap
+        rot_times = [val*-1 for val in fresh.values()]
+        heapq.heapify(rot_times)
+        if rot_times[0]*-1 is 11: return -1
+        else: return rot_times[0]*-1
