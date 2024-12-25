cbe17bcc980cd5023d7368e513cdfca8b6388ad2
Derek Gomez <demagome@ucsc.edu>
2024-12-25 00:28:27 -0800

    COMMIT Capitalize the Title

---
+++ Capitalize the Title/Capitalize the Title.py
@@ -0,0 +1,46 @@
+class Solution:
+    def capitalizeTitle(self, title: str) -> str:
+        
+        ans = ""
+
+        wordStart = -1
+        for i in range(len(title)):
+            # beginning of word
+            if wordStart < 0 and title[i] != " ":
+                wordStart = i
+
+            # end of word
+            elif i == len(title)-1:
+                if i - wordStart <= 2:
+                    # lowercase because word len <= 2
+                    for j in range(wordStart, i):
+                        ans += title[j].lower()
+                         
+                else:
+                    # uppercase beginning of word
+                    ans += title[wordStart].upper()
+
+                    #lowecase the rest of the word
+                    for j in range(wordStart+1, i+1):
+                        ans += title[j].lower()
+                
+                wordStart = -1
+            
+            elif title[i] == " ":
+                if i - wordStart <= 2:
+                    # lowercase because word len <= 2
+                    for j in range(wordStart, i):
+                        ans += title[j].lower()
+                else:
+                    # uppercase beginning of word
+                    ans += title[wordStart].upper()
+
+                    #lowercase the rest of the word
+                    for j in range(wordStart+1, i):
+                        ans += title[j].lower()
+                
+                ans += " "
+                wordStart = -1
+
+
+        return ans
