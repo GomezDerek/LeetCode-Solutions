51f564ad9250425f376b8605471c86e1fa03fdeb
Derek Gomez <demagome@ucsc.edu>
2024-12-26 15:43:38 -0800

    COMMIT Target Sum

---
+++ Target Sum/Target Sum.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        # solution will be O(2^N)
+        # recursively traverse and branch through the index
+        # each branches with a negative or positive
+        # there's probably an opportunity for optimization with dynamic programming
+
+        ans = 0
+
+        def r(prog_sum, i):
+            # base case
+            print(i)
+            if i >= len(nums): # finished array traversal
+                if prog_sum == target: # sumWay found
+                    nonlocal ans
+                    ans += 1
+                return
+
+            # print(i, nums[i])
+
+            # recursive call(s)
+            r(prog_sum + nums[i], i+1)
+            r(prog_sum - nums[i], i+1) 
+
+        r(0,0)
+        return ans
