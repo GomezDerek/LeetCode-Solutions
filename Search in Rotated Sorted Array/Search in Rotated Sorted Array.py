d66fd5c0ae615ea4288231b2ad1ead67f65286a4
Derek Gomez <demagome@ucsc.edu>
2024-12-26 15:04:33 -0800

    COMMIT Search in Rotated Sorted Array

--- Search in Rotated Sorted Array/Search in Rotated Sorted Array.py
+++ Search in Rotated Sorted Array/Search in Rotated Sorted Array.py
@@ -1,32 +1,32 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # Neetcode solution
+        # Neetcode solution from memory
 
         l, r = 0, len(nums)-1
 
-        while l <= r:
+        while(l <= r):
             m = (l+r)//2
-
+            
             # target found
-            if target == nums[m]:
+            if nums[m] == target:
                 return m
+            
+            # m in left of partition
+            elif nums[l] < nums[m]:
+                if target < nums[l]:
+                    l = m+1
+                elif target > nums[m]:
+                    l = m+1
+                else: # nums[l] < target < nums[m]
+                    r = m-1
 
-            # left sorted portion
-            if nums[l] <= nums[m]:    # left < mid
-                if target > nums[m]:    # target > mid
-                    l = m + 1               # search nums[m:r]
-                elif target < nums[l]:  # target < left
-                    l = m + 1               # search nums[m:r]
-                else:                   # left < target < middle
-                    r = m - 1               # search nums[l:m]
-
-            # right sorted portion
-            else:                       # left >= mid
-                if target < nums[m]:        # target < mid
-                    r = m -1                    # search nums[l:m]
-                elif target > nums[r]:      # target > right
-                    r = m - 1                   # search nums[l:m]
-                else:                   # middle < target < right
-                    l = m + 1               # search nums[m:r]
+            # m in right of partition
+            else:
+                if target > nums[r]:
+                    r = m-1
+                elif target < nums[m]:
+                    r = m-1
+                else: # nums[m] < target < nums[r]
+                    l = m + 1
 
         return -1
