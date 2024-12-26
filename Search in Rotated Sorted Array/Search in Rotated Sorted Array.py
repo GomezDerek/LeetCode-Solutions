d65ca10aef522af6939edf0d17457d474e6737eb
Derek Gomez <demagome@ucsc.edu>
2024-12-26 14:55:40 -0800

    COMMIT Search in Rotated Sorted Array

--- Search in Rotated Sorted Array/Search in Rotated Sorted Array.py
+++ Search in Rotated Sorted Array/Search in Rotated Sorted Array.py
@@ -1,76 +1,32 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # find pivot?
-        # binary search on either side of pivot?
+        # Neetcode solution
 
-        pivot = None
+        l, r = 0, len(nums)-1
 
-        l = 0
-        r = len(nums)-1
-        while( l < r):
+        while l <= r:
             m = (l+r)//2
 
-            # pivot found!
-            if nums[m] < nums[m-1]:
-                pivot = m
-                break
-            elif nums[m] > nums[m+1]:
-                pivot = m+1
-                break
+            # target found
+            if target == nums[m]:
+                return m
 
-            # no pivot found
+            # left sorted portion
+            if nums[l] <= nums[m]:    # left < mid
+                if target > nums[m]:    # target > mid
+                    l = m + 1               # search nums[m:r]
+                elif target < nums[l]:  # target < left
+                    l = m + 1               # search nums[m:r]
+                else:                   # left < target < middle
+                    r = m - 1               # search nums[l:m]
 
-            # is pivot between l and r?
-            if nums[l] > nums[r]:
-                l = (l+m)//2
-                r = (r+m)//2
-                continue
-            # or did we pass the pivot?
-            else:
-                # print("pivot passed", l, r)
-                break
-                # return
+            # right sorted portion
+            else:                       # left >= mid
+                if target < nums[m]:        # target < mid
+                    r = m -1                    # search nums[l:m]
+                elif target > nums[r]:      # target > right
+                    r = m - 1                   # search nums[l:m]
+                else:                   # middle < target < right
+                    l = m + 1               # search nums[m:r]
 
-
-        # print("pivot:", pivot)
-
-        unpivoted = None
-        if not pivot:
-            unpivoted = nums
-        
-        # unrotate array
-        else:
-            unpivoted = nums[pivot:] + nums[:pivot]
-        # print(unpivoted)
-
-        def calc_ind_w_k(m):
-            # if value is after pivot in nums
-            if nums[pivot] <= unpivoted[m] <= nums[-1]:
-                return m + pivot
-
-            # if value is before pivot in nums
-            elif nums[0] <= unpivoted[m] <= nums[pivot-1]:
-                return (m + pivot)%len(nums)
-        # binary search on unpivoted
-        l = 0
-        r = len(nums) - 1
-        while (l <= r):
-            m = (l+r)//2
-            # print(f"m: {m}\n[m]: {unpivoted[m]}")
-            
-            # target found! We win!
-            if unpivoted[m] == target:
-                # print("target found!")
-                return (m + pivot)%len(nums) if pivot else m
-                # return calc_ind_w_k(m) if pivot else m
-
-            elif unpivoted[m] < target:
-                l = m+1
-                continue
-
-            elif unpivoted[m] > target:
-                r = m-1
-                continue
-
-        # target DNE
         return -1
