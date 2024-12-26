b1e0b61a977437cdc433a273a75304c118c8eac4
Derek Gomez <demagome@ucsc.edu>
2024-12-26 06:40:03 -0800

    COMMIT Find Smallest Letter Greater Than Target

--- Find Smallest Letter Greater Than Target/Find Smallest Letter Greater Than Target.py
+++ Find Smallest Letter Greater Than Target/Find Smallest Letter Greater Than Target.py
@@ -2,8 +2,7 @@ class Solution:
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
         ans = letters[0]
 
-        for i, ch in enumerate(letters):
-            if ch > target:
-                return ch
+        for ch in letters:
+            if ch > target: return ch
     
         return ans
