ddc9e052ed04619f63613d2547b417f2af08e79d
Derek Gomez <demagome@ucsc.edu>
2024-12-26 16:54:40 -0800

    COMMIT Target Sum

--- Target Sum/Target Sum.py
+++ Target Sum/Target Sum.py
@@ -1,54 +1,21 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        # solution will be O(2^N)
-        # recursively traverse and branch through the index
-        # each branches with a negative or positive
-        # there's probably an opportunity for optimization with dynamic programming
-
-        # return len(nums)
-
-        ans = 0
+        # neetcode solution 
 
         cache = {}
 
-        def r(prog_sum, i):
-            # base case
-            # print(i)
-            if i == len(nums)-1:
-                nonlocal ans
-                if prog_sum + nums[i] == target: ans += 1
-                if prog_sum - nums[i] == target: ans += 1
-                return
+        def recursion(i, prog_sum):
+            # base case(s)
+            if i == len(nums):
+                return 1 if prog_sum == target else 0
 
-            # print(i, nums[i])
+            if (i, prog_sum) in cache:
+                return cache[(i, prog_sum)]
 
             # recursive call(s)
-            for num in [nums[i], -nums[i]]:
-                # print(cache)
-                # i_cache = cache[(prog_sum, num)]
-                try:
-                    r(cache[(prog_sum, num)], i+1)
-                
-                except KeyError:
-                    cache[(prog_sum, num)] = prog_sum + num
-                    r(cache[(prog_sum, num)], i+1)
+            cache[(i, prog_sum)] = recursion(i+1, prog_sum + nums[i]) + recursion(i+1, prog_sum - nums[i])
 
-                # if (prog_sum, num) in cache:
-                #     print((prog_sum, num), "cache called!")
-                #     # r(i_cache, i+1)
-                #     r(cache[(prog_sum, num)], i+1)
+            # return
+            return cache[(i, prog_sum)]
 
-                # else:
-                #     cache[(prog_sum, num)] = prog_sum + num
-                #     r(cache[(prog_sum, num)], i+1)
-            # r(prog_sum + nums[i], i+1)
-            # r(prog_sum - nums[i], i+1) 
-
-        r(0,0)
-        return ans
-
-
-        #                                                   0
-        #                        [-49]                                               [+49]
-        #        [-49 +19]                [-49 -19]                   [+49 +19]                  [+49 -19]
-        # [-49 +19 +9] [-49 +19 -9] [-49 -19 +9] [-49 -19 -9] [+49 +19 +9] [+49 +19 -9] [+49 - 19 +9] [+49 - 19 -9]
+        return recursion(0,0)
