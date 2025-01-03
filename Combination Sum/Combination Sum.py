2eb1813130464c02ae2d43b01892bd9af742d628
Derek Gomez <demagome@ucsc.edu>
2025-01-02 18:41:08 -0800

    COMMIT Combination Sum

--- Combination Sum/Combination Sum.py
+++ Combination Sum/Combination Sum.py
@@ -1,42 +1,23 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        # strategy
-        # step 1: calc factorials for target
-        # step 2: find factorials in candidates
-        # step 3: calc combinations
-            # combo a: consists of only one factorial (i.e. 2)
-            # combo b: consists of multiple factorials (i.e. 2,3)
-            # combo c: higher factorials included (i.e. 4)
+        # Neetcode solution
 
-        # no combinations if
-        # no factorials are in candidates[]
+        combinations = []
 
+        def dfs(i, currCombo, total):
+            # successful base case
+            if total == target:
+                combinations.append(currCombo[:]) # append a copy instead of a reference
+                return
 
-        # new strategy
-        # O(n^2) runtime
-        # for each num in candidates: try to make combinations with every other candidate
+            # failed base case
+            elif i >= len(candidates) or total > target:
+                return
 
+            # recursive call(s)
+            else:
+                dfs(i, currCombo + [candidates[i]], total + candidates[i]) # branch to a duplicate of current candidate
+                dfs(i+1, currCombo, total) # branch to the next candidate
 
-        combinations = {}
-
-        for n, num in enumerate(candidates):
-            if num > target:
-                continue
-
-            for i in range(1, target//num+1):
-                # combo a
-                if i*num == target:
-                    combo = [num for _ in range(target//num)]
-                    sorted_tup = tuple( x for x in sorted(combo) )
-                    combinations[sorted_tup] = combo
-
-                else:
-                    diff = target - i*num
-                    if diff in candidates:
-                        combo = [num for _ in range(i)] + [diff]
-                        sorted_tup = tuple( x for x in sorted(combo))
-                        
-                        if sorted_tup not in combinations.keys():
-                            combinations[sorted_tup] = combo
-
-        return [ value for value in combinations ]
+        dfs(0,[],0)
+        return combinations
