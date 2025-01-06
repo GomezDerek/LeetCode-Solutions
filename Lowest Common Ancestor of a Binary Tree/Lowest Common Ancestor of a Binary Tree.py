fcb2bd8188c963a52c9237beb3287138a12cd6fe
Derek Gomez <demagome@ucsc.edu>
2025-01-06 13:24:27 -0800

    COMMIT Lowest Common Ancestor of a Binary Tree

--- Lowest Common Ancestor of a Binary Tree/Lowest Common Ancestor of a Binary Tree.py
+++ Lowest Common Ancestor of a Binary Tree/Lowest Common Ancestor of a Binary Tree.py
@@ -8,23 +8,22 @@
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
 
-        paths = {}
-        pFound, qFound = False, False
+        pPath, qPath = [], []
 
         def dfs(node, path):
             # base case
-            nonlocal pFound, qFound
-            if not node or (pFound is True and qFound is True):
+            nonlocal pPath, qPath
+            if not node or (pPath is False and qPath is False):
                 return
             
-            # check if we found our targets
-            elif node is p:
-                pFound = True
-            elif node is q:
-                qFound = True
 
             newPath = path + [node]
-            paths[node] = newPath
+            
+            # check if we found our targets
+            if node is p:
+                pPath = newPath
+            elif node is q:
+                qPath = newPath
 
             dfs(node.left, newPath)
             dfs(node.right, newPath)
@@ -32,8 +31,6 @@ class Solution:
         dfs(root, [])
 
         # find LCA by comparing paths
-        pPath = paths[p]
-        qPath = paths[q]
         pPath.reverse()
         qPath.reverse()
 
