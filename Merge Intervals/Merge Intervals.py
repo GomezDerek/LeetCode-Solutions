c12a204106efb103675ce6e4669a367a9961226a
Derek Gomez <demagome@ucsc.edu>
2025-01-06 00:32:17 -0800

    COMMIT Merge Intervals

--- Merge Intervals/Merge Intervals.py
+++ Merge Intervals/Merge Intervals.py
@@ -1,37 +1,15 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        # inspired by Greg Hogg's solution
+
+        intervals.sort(key= lambda interval: interval[0])
         
         answer = [intervals[0]]
 
-        for i in range(1, len(intervals)):
-            interval = intervals[i]
-
-            print(answer[-1], interval)
-            # if answer[-1][0] < interval[0] and answer[-1][1] < interval[0]:
-            #     answer.append(interval)
-            #     continue
-            merged = False
-            while answer[-1][1] >= interval[0] and answer[-1][0] <= interval[1]:
-                answer[-1][1] = max(answer[-1][1], interval[1])
-                answer[-1][0] = min(answer[-1][0], interval[0])
-                
-                # all intervals have been merged so far
-                if answer[-1] is answer[0]:
-                    merged = True
-                    break
-                else:
-                    interval = answer.pop()
-            
-            if not merged:
+        for interval in intervals[1:]:
+            if interval[0] <= answer[-1][1]:
+                answer[-1][1] = max(interval[1], answer[-1][1])
+            else:
                 answer.append(interval)
-                               
-            
-            # intervals do not overlap
-
-            # else:
-            #     # update answer and reset curr
-            #     answer.append(currInterval)
-            #     currInterval = intervals[i]
-        
 
         return answer
