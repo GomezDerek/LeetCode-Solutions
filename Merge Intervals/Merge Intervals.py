10fafdfb5741f735034aa0fa24d875a20a47f659
Derek Gomez <demagome@ucsc.edu>
2025-01-05 23:00:22 -0800

    COMMIT Merge Intervals

--- Merge Intervals/Merge Intervals.py
+++ Merge Intervals/Merge Intervals.py
@@ -5,6 +5,8 @@ class Solution:
                 # YES, THEY CAN BE UNSORTED
             # can intervals be sub intervals of each other
             # can startI ?= endI
+                # YES
+            # description doesn't say the answer doesn't need to be sorted...
 
         # strategy
         # keep track of an interval as we iterate through the list
@@ -19,8 +21,11 @@ class Solution:
             # print(intervals[i])
 
             # if intervals overlap, merge it into currInterval
-            # if curr's end > interval's start
-            if currInterval[1] >= intervals[i][0]:
+            # rewrite this because it's intervals are not sorted!
+            # if currInterval[1] >= intervals[i][0]:
+            # [1,4][4,5]     [1,4][0,4]    [1,4][0,1]   [1,4][0,0]
+            # [1,6] [8,10]
+            if currInterval[1] >= intervals[i][0] and currInterval[0] <= intervals[i][1]:
                 # merge by updating curr's end
                 currInterval[1] = max(currInterval[1], intervals[i][1])
                 currInterval[0] = min(currInterval[0], intervals[i][0])
