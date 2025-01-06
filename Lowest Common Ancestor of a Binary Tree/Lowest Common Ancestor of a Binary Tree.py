79659dd3cadf95f91e65513c6334f7dda3c48cb8
Derek Gomez <demagome@ucsc.edu>
2025-01-06 12:56:03 -0800

    COMMIT Lowest Common Ancestor of a Binary Tree

--- Lowest Common Ancestor of a Binary Tree/Lowest Common Ancestor of a Binary Tree.py
+++ Lowest Common Ancestor of a Binary Tree/Lowest Common Ancestor of a Binary Tree.py
@@ -7,50 +7,47 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        # strategy
-        # DFS traversal to both nodes
-        # record the paths to each node
-        # find LCA by comparing paths
-            # LCA found by the last shared node in path
-
-        # RESTRATEGY (I realized the tree is not balanced)
+        # STRATEGY
+        # consider the tree is not balanced
         # BFS traversal
-        # record paths to EVERY node with a hashmap
-        # once both targets have been found, end traversal
+            # record paths to EVERY node with a hashmap
+            # once both targets have been found, end traversal
         # find LCA by comparing paths
             # LCA found by the last shared node in path
 
         queue = collections.deque()
         queue.append( [root, []] )
         paths = {}
+        pFound, qFound = False, False
 
-        while queue:
+        # end traversal early when both targets have been found
+        while queue or not pFound or not qFound:
             node, path = queue.popleft()
-            if node:
-                # print(node.val, path)
-                newPath = path + [node]
-                paths[node] = newPath
-                # paths[node.val] = newPath
+
+            # base case
+            # skip this node if null
+            if not node:
+                continue
+            
+            else:
+                newPath = path + [node] # update path
+                paths[node] = newPath   # add node's path to hashmap
+                
+                if node.val == p.val:
+                    pFound = True
+                elif node.val == q.val:
+                    qFound = True
+
                 queue.append([node.left, newPath])
                 queue.append([node.right, newPath])
-            else:
-                # print(None, path)
-                continue
-
-            
+                        
         # find LCA by comparing paths
-
         pPath = paths[p]
         qPath = paths[q]
-        # pPath = paths[p.val]
-        # qPath = paths[q.val]
-        # print(pPath, qPath)
         pPath.reverse()
         qPath.reverse()
-        # print(pPath, qPath)
 
         for pNode in pPath:
             for qNode in qPath:
-                # print(pNode, qNode)
-                if pNode.val == qNode.val:
+                if pNode.val == qNode.val: 
                     return pNode
