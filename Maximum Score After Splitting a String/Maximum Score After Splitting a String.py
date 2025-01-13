8bbc2cb1804f07bfe4967094d47ffc5258be6c8e
Derek Gomez <demagome@ucsc.edu>
2025-01-13 00:27:48 -0800

    COMMIT Maximum Score After Splitting a String

--- Maximum Score After Splitting a String/Maximum Score After Splitting a String.py
+++ Maximum Score After Splitting a String/Maximum Score After Splitting a String.py
@@ -1,26 +1,24 @@
 class Solution:
     def maxScore(self, s: str) -> int:
-        # strategy
-        # create n # of splits and calc scores linearly O(n^2)
+        # optimization attempt #2
+        
+        zedCount = []
 
-        # edge cases
-        # constraints don't warrant any
+        for i, num in enumerate(s):
+            if i == 0:
+                zedCount.append( 1 if num == "0" else 0 )
+            else:
+                zedCount.append( zedCount[-1]+0 if num == "1" else zedCount[-1]+1 )
+        
 
+        oneCount = 0
         maxScore = 0
-
         for i in range(len(s)-1):
-            left = s[:i+1]
-            right = s[i+1:]
-
-            score = 0
-
-            for num in left:
-                score += 1 if num == "0" else 0
+            oneCount += 1 if s[i] == "1" else 0
+            leftScore = zedCount[i]
+            rightScore = len(s) - zedCount[-1] - oneCount
             
-            for num in right:
-                score += 1 if num == "1" else 0
-
-            maxScore = score if score > maxScore else maxScore
-            # print(left, right, score)
+            newScore = leftScore + rightScore
+            maxScore = newScore if newScore > maxScore else maxScore
 
         return maxScore
