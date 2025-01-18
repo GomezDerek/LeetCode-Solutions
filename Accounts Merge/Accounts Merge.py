704677d28ac11a4dcb4a3eb9535dc68cf4144651
Derek Gomez <demagome@ucsc.edu>
2025-01-17 23:59:44 -0800

    COMMIT Accounts Merge

--- Accounts Merge/Accounts Merge.py
+++ Accounts Merge/Accounts Merge.py
@@ -1,28 +1,41 @@
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
-        # strategy 
-        # data structure = { 
-        #   sharedName1: [ 
-        #       [ email1, email2, ... ],
-        #       [ email3, email4, ... ],
-        #   ],
-        #   sharedName2: [
-        #       ...
-        #   ]
+        # name {
+        #     [
+        #       { email1, email2, ... },
+        #       { email3, email4, ... }
+        #     】
         # }
 
-        # Step 1: populate the hashmap
-        # Step 2: merge the arrays
+        hashMap = {}
 
-        # Step 3: optimize to merge during insertion
-        answer = {}
+        for arr in accounts:
+            name = arr[0]
+            emails = arr[1:]
+            # print(f"{name}\n{emails}\n")
 
-        for account in accounts:
-            name, emails = account[0], account[1:]
-            if answer.get(name):
-                answer[name].append(emails)
+            if hashMap.get(name):
+                
+                # check if they should be merged
+                unmergedSets = []
+                mergedSet = set(emails)
+                
+                emailSets = hashMap[name]
+                for emailSet in emailSets:
+                    if not emailSet.isdisjoint(emails):
+                        # print(f"merge needed between:\n{emailSet}\n{emails}")
+                        mergedSet.update(emailSet)
+                    else:
+                        unmergedSets.append(emailSet)
+
+                hashMap[name] = unmergedSets + [mergedSet]
+
             else:
-                answer[name] = [emails]
+                hashMap[name] = [set(emails)]
 
-        print(answer)
-        return answer
+        # print(hashMap)
+        formattedAnswer = []
+        for name, emailSets in hashMap.items():
+            for emailSet in emailSets:
+                formattedAnswer.append([name] + sorted(emailSet))
+        return formattedAnswer
