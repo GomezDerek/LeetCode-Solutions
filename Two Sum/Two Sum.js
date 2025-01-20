8a9a857ea67ee33ee27160076924a0ef3243f54b
Derek Gomez <demagome@ucsc.edu>
2025-01-19 20:14:55 -0800

    COMMIT Two Sum

--- Two Sum/Two Sum.js
+++ Two Sum/Two Sum.js
@@ -4,21 +4,25 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    hashMap = {}
-    
-    // populate hashMap
-    nums.forEach( (num, i) => {
-        hashMap[num] = i
+    // solution inspired by Neetcode hint
+
+    const hashMap = new Map();
+
+    var ans = null;
+    nums.some( (num, i) => {
+        const diff = target - num
+        
+        // solution found
+        if(hashMap.has(diff)) {
+            ans = [i, hashMap.get(diff)]
+            return true;
+        }
+
+        // solution not found yet
+        else {
+            hashMap.set(num, i)
+        }
     });
 
-    // console.log(hashMap)
-
-    // iterate for solution
-    for(let i=0; i < nums.length; i++) {
-        diff = target - nums[i]
-        // console.log(`i:${i}\nnum:${nums[i]}\ndiff:${diff}\nhashMap[diff]:${hashMap[diff]}`);
-        if (hashMap[diff] != undefined && hashMap[diff] != i) {
-            return [i, hashMap[diff]]
-        }
-    }
+    return ans
 };
