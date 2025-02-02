2facebfabb392f872f58446432cb95d9b9e763b5
Derek Gomez <demagome@ucsc.edu>
2025-02-01 17:49:00 -0800

    COMMIT Longest Consecutive Sequence

--- Longest Consecutive Sequence/Longest Consecutive Sequence.js
+++ Longest Consecutive Sequence/Longest Consecutive Sequence.js
@@ -3,44 +3,38 @@
  * @return {number}
  */
 var longestConsecutive = function(nums) {
-    // naive solution, O(4N) -> O(N)
-    // use a hashMap of booleans
-    // key: num: string, value: exists:boolean
-
-    // 1st iterate to find min/max value
-    // 2nd iteration to set keys for hashMap 
-    // 3rd iteration to populate hashMap
-    // 4th iteration to find longest sequence
-
-    let minVal = 1000000000;
-    let maxVal = -1000000000;
-    for(const num of nums) {
-        if (num < minVal) minVal = num
-        else if (num > maxVal) maxVal = num 
-    }
-    // console.log(minVal, maxVal);
-
+    // BFS solution inspired from discussion section
     const hMap = new Map();
-    for(let i=minVal; i<=maxVal; i++) {
-        hMap.set(i, false);
-    }
-    // console.log(hMap);
 
+    // populate hashMap
     for(const num of nums) {
-        hMap.set(num, true);
+        hMap.set(num, null);
     }
-    // console.log(hMap);
 
     let maxStreak = 0;
     let currStreak = 0;
 
-    for(const val of hMap.values()) {
-        if (!val) currStreak = 0;
+    for(const key of hMap.keys()) {
+
+        // if num isn't in the middle/end of a subsequence
+        // then we can start streak
+        if ( !hMap.has(key-1) ) {
+            
+            let currKey = key;
+            while( hMap.has(currKey) ) {
+                currStreak ++;
+                maxStreak = currStreak > maxStreak ? currStreak : maxStreak;
+
+                hMap.delete(currKey);
+                currKey++;    
+            }
+            currStreak = 0;
+        }
+
+        // num can't start a sequence
         else {
-            currStreak ++;
-            maxStreak = currStreak > maxStreak ? currStreak : maxStreak;
+            continue;
         }
     }
-
     return maxStreak;
 };
