87f061d742150915735cc26c41a86a47e8d09dd5
Derek Gomez <demagome@ucsc.edu>
2025-02-06 14:18:59 -0800

    COMMIT 3Sum

--- 3Sum/3Sum.js
+++ 3Sum/3Sum.js
@@ -14,34 +14,45 @@ var threeSum = function(nums) {
     // to return the answer
     // convert set to array, and then convert the strings into number[]
 
+    // TLE counter -> hashmap
+
 
     const pairs = [];
+    const hMap = {};
     const triplets = new Set();
 
     // create every possible pair of vals
     for(let i=0; i<nums.length; i++) {
+        const val = nums[i];
+        if ( !hMap[val] ) {
+            hMap[val] = [i];
+        }
+        else {
+            hMap[val].push(i);
+        }
         for(let j=i+1; j<nums.length; j++) {
-            pairs.push( [nums[i], nums[j]] );
+            pairs.push( [i,j] );
         }
     }
-    
-    // try to find valid triplets for every pair
-    pairs.forEach( ([a,b]) => {
-        
-        // iterate through the entire num[] for the 3rd element
-        nums.forEach( c => {
-            // skip to avoid duplicate values
-            if ( [a,b].includes(c) ) {
-                return;
-            }
 
-            // valid triplet found!
-            if (a+b+c == 0) {
-                // sort, stringify, and store!
-                const triplet = [a,b,c].sort().toString();
-                triplets.add( triplet );
-            }
-        });
+    // // try to find valid triplets for every pair
+    pairs.forEach( ([i,j]) => {
+        const [a,b] = [i,j].map(n => nums[n]); // pair's values
+        const c = 0-a-b; // last triplet value
+
+        // third value doesn't exist in nums
+        if ( !hMap[c] ) {
+            return;
+        }
+        
+        const cIndices = hMap[c]; // all indices in nums that store value c
+        
+        // if no duplicate indices
+        if ( cIndices.some(k => ![i,j].includes(k)) ) {
+            const triplet = [a,b,c].sort().toString();
+            triplets.add(triplet);
+        }
+
     });
 
     return [...triplets].map( s => s.split(',').map( n => parseInt(n)) );
