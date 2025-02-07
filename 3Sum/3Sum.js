1b7ce52df25399fff1ae947f23c13b7e44113ddf
Derek Gomez <demagome@ucsc.edu>
2025-02-07 01:28:12 -0800

    COMMIT 3Sum

--- 3Sum/3Sum.js
+++ 3Sum/3Sum.js
@@ -3,55 +3,59 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    // naive strategy
-    // create a list of every possible pair 
-    // for every pair, try to find a 3rd index
-    // time complexity: O(n^2) * O(n) -> O(n^3)
+    nums.sort();
+    const ans = [];
 
-    // how to prevent duplicates??
-    // sort, stringify, and then store the values into a set
-
-    // to return the answer
-    // convert set to array, and then convert the strings into number[]
-
-    // TLE counter -> hashmap
-
-
-    const pairs = [];
-    const hMap = {};
-    const triplets = new Set();
-
-    // create every possible pair of vals
     for(let i=0; i<nums.length; i++) {
-        
-        const val = nums[i];
-        if ( !hMap[val] ) hMap[val] = [i];
-        else hMap[val].push(i);
+        if (nums[i] > 0) break;
 
-        for(let j=i+1; j<nums.length; j++) {
-            pairs.push( [i,j] );
+        // skip duplicates
+        if (i > 0 && nums[i] === nums[i-1]) {
+            continue;
+        }
+    
+        // solve for two-sum
+        let l = i+1;
+        let r = nums.length-1;
+
+        while(l<r) {
+            const sum3 = nums[i] + nums[l] + nums[r];
+
+            // twosum is too big
+            if (sum3 > 0)   {
+                r = decrementR(nums, r);
+            }
+            // twosum is too small
+            else if (sum3 < 0) {
+                l = incrementL(nums, l);
+            }
+
+            // twosum is just right
+            else if (sum3 == 0)  {
+                const triplet = [nums[i],nums[l],nums[r]];
+                ans.push(triplet);
+                
+                // continue looking for valid triplets for this i
+                r = decrementR(nums,r);
+                l = incrementL(nums, l);
+            }
         }
     }
-
-    // // try to find valid triplets for every pair
-    pairs.forEach( ([i,j]) => {
-        const [a,b] = [i,j].map(n => nums[n]); // pair's values
-        const c = 0-a-b; // last triplet value
-
-        // third value doesn't exist in nums
-        if ( !hMap[c] ) {
-            return;
-        }
-        
-        const cIndices = hMap[c]; // all indices in nums that store value c
-        
-        // if no duplicate indices
-        if ( cIndices.some(k => ![i,j].includes(k)) ) {
-            const triplet = [a,b,c].sort().toString();
-            triplets.add(triplet);
-        }
-
-    });
-
-    return [...triplets].map( s => s.split(',').map( n => parseInt(n)) );
+    return ans;
 };
+
+var decrementR = function(nums,r) {
+    r--;
+    while(nums[r] == nums[r+1]) {
+        r--;
+    }
+    return r;
+}
+
+var incrementL = function(nums,l) {
+    l++;
+    while(nums[l] == nums[l-1]) {
+        l++;
+    }
+    return l;
+}
