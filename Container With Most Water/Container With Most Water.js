750fa08a5a252c0716d92a9b5ef7709d63555bf4
Derek Gomez <demagome@ucsc.edu>
2025-02-11 10:04:05 -0800

    COMMIT Container With Most Water

---
+++ Container With Most Water/Container With Most Water.js
@@ -0,0 +1,49 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    // strategy
+    // identify tallest heights and calculate water volume
+
+    // brute force solution O(n^2)
+    // calculate all possible volumes
+
+    // optimized approach O(n)
+    // two pointer solution, [0] & [length]
+    // move pointers to center until max volume is found
+
+    let l = 0;
+    let r = height.length -1;
+
+    let maxVol = 0;
+    let currVol;
+    
+    while(l<r) {
+
+        let shorter;
+        if(Math.min(height[l], height[r]) == height[l]) {
+            shorter = l
+        }
+        else {
+            shorter = r;
+        }
+
+        // h X w
+        currVol = height[shorter] * (r-l);
+        maxVol = Math.max(maxVol, currVol);
+        // console.log(l,r, currVol);
+
+        if(shorter == l) {
+            l++;
+        }
+        else if (shorter == r) {
+            r--;
+        }
+        else {
+            console.log('alert');
+        }
+    }
+
+    return maxVol;
+};
