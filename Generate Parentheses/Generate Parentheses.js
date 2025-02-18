3227449d979685e75d0eb6b35e881d8d43cb4eda
Derek Gomez <demagome@ucsc.edu>
2025-02-18 09:06:30 -0800

    COMMIT Generate Parentheses

--- Generate Parentheses/Generate Parentheses.js
+++ Generate Parentheses/Generate Parentheses.js
@@ -34,13 +34,25 @@ var generateParenthesis = function(n) {
         // iterate through prev
         for(const combo of answer[i-1]) {
             // add new combos to nCombos
-            nCombos.add( combo+'()' );
-            nCombos.add( '()'+combo );
             nCombos.add('('+combo+')');
+            nCombos.add( '()'+combo );
+            nCombos.add( combo+'()' );
+        }
+
+        if(i >= 4) {
+            let combo = [...answer[i-2]][0];
+            nCombos.add(combo+combo);
         }
 
         // add nCombos to our answer
         answer.push(nCombos);
+    }
+
+    const answer4 = ["(((())))","((()()))","((())())","((()))()","(()(()))","(()()())","(()())()","(())(())","(())()()","()((()))","()(()())","()(())()","()()(())","()()()()"];
+
+    if(n==4) {
+        let missing = answer4.filter(combo => !answer[4].has(combo) );
+        console.log(missing)
     }
 
     // convert our set to an array
