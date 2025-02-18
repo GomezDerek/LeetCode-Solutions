03a78f3099fffd326197134ed4ab7534b0df50f0
Derek Gomez <demagome@ucsc.edu>
2025-02-18 11:43:22 -0800

    COMMIT Daily Temperatures

--- Daily Temperatures/Daily Temperatures.js
+++ Daily Temperatures/Daily Temperatures.js
@@ -2,23 +2,55 @@
  * @param {number[]} temperatures
  * @return {number[]}
  */
-var dailyTemperatures = function(temperatures) {
-    // naive solution 
-    // O(n^2) runtime
-    // for each day, iterate through the rest of the string to find the warmer day
+var dailyTemperatures = function (temperatures) {
+    // backward iteration with descending monotonic stack
+    const stack = [];
+    const answer = new Array(temperatures.length);
 
-    const answer = [];
-    
-    for(let i=0; i<temperatures.length; i++) {
-        let daysToWait = 0;
-        for(let j=i+1; j<temperatures.length; j++) {
-            if(temperatures[j] > temperatures[i]) {
-                daysToWait = j-i;
-                break;
+    // because we know last element is ALWAYS 0
+    answer[answer.length - 1] = 0;
+    stack.push( temperatures.length-1 );
+
+    for (let i = temperatures.length - 2; i >= 0; i--) {
+        let stackIndex = stack.length - 1;
+        let stackVal = temperatures[stack[stackIndex]];
+
+        let daysUntilHotter = 0;
+
+        if (!(stackVal < temperatures[i])) { // we found a temperature that is hotter than current
+            daysUntilHotter = stack[stackIndex] - i;
+        }
+        else {
+            while (stack.length) { // while stack has something
+                stack.pop();
+
+                if(!stack.length) {
+                    break;
+                }
+
+                // recalculate after pop()
+                stackIndex = stack.length - 1;
+                stackVal = temperatures[stack[stackIndex]];
+
+                if (!(stackVal < temperatures[i])) { // we found a temperature that is hotter than current
+                    daysUntilHotter = stack[stackIndex] - i;
+                    break;
+                }
             }
         }
-        answer.push(daysToWait);
+
+        answer[i] = daysUntilHotter;
+        stack.push(i);
+
+        // if !(stackTopVal < currVal)
+        //      answer[i] = stackIndex - i;
+        // }
+        // else {
+        //  stack.pop()
+        //  compare the new stackTopVal with currVal
+        // if stack is empty, then
+        // answer[i] = 0;
     }
-    
+
     return answer;
 };
