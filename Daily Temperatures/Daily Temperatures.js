024edc5bb37508dbf68704ef984cdbc2daa9da85
Derek Gomez <demagome@ucsc.edu>
2025-02-18 16:54:29 -0800

    COMMIT Daily Temperatures

--- Daily Temperatures/Daily Temperatures.js
+++ Daily Temperatures/Daily Temperatures.js
@@ -5,27 +5,30 @@
 var dailyTemperatures = function (temperatures) {
 
     // forward iteration with ascending monotonic stack
-    const stack = [0];
+    // stack stores [i, temp]
+    const stack = [];
     const answer = new Array(temperatures.length).fill(0);
 
-    for(let i=1; i<temperatures.length; i++) {
+    for(let i=0; i<temperatures.length; i++) {
         const currTemp = temperatures[i];
 
         // while stack not empty
         while(stack.length) {
-            const stackTop = stack[stack.length-1];
-            const stackTemp = temperatures[stackTop];
+            // const stackTop = stack[stack.length-1];
+            // const stackTemp = temperatures[stackTop];
+            const [stackIndex, stackTemp] = stack[stack.length-1];
 
             // currTemp is hotter than stack top's temperature
             if( currTemp > stackTemp ) {
-                answer[stackTop] = i - stackTop;
+                answer[stackIndex] = i - stackIndex;
+                // answer[stackTop] = i - stackTop;
                 stack.pop(); // remove stackTemp from stack
             }
             else break; // no chance currTemp can be hotter than the rest of the stack
         }
 
         // always add currTempIndex to stack
-        stack.push(i);
+        stack.push([i, currTemp]);
     }
 
     return answer;
