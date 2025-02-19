a2a80d69e39467b4d8e9bee2bf4594f4440ef3a2
Derek Gomez <demagome@ucsc.edu>
2025-02-18 17:04:42 -0800

    COMMIT Daily Temperatures

--- Daily Temperatures/Daily Temperatures.js
+++ Daily Temperatures/Daily Temperatures.js
@@ -9,16 +9,13 @@ var dailyTemperatures = function (temperatures) {
     const answer = new Array(temperatures.length).fill(0);
 
     for(let i=0; i<temperatures.length; i++) {
-        const currTemp = temperatures[i];
 
         // while stack not empty
         while(stack.length) {
-            const stackTop = stack[stack.length-1];
-            const stackTemp = temperatures[stackTop];
 
             // currTemp is hotter than stack top's temperature
-            if( currTemp > stackTemp ) {
-                answer[stackTop] = i - stack.pop();
+            if( temperatures[i] > temperatures[ stack[stack.length-1] ] ) {
+                answer[ stack[stack.length-1] ] = i - stack.pop();
             }
             else break; // no chance currTemp can be hotter than the rest of the stack
         }
