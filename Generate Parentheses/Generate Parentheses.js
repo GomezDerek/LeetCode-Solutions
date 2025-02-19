26ad950dcdd950986a85da0bcb3304c55a4adac8
Derek Gomez <demagome@ucsc.edu>
2025-02-19 11:38:35 -0800

    COMMIT Generate Parentheses

--- Generate Parentheses/Generate Parentheses.js
+++ Generate Parentheses/Generate Parentheses.js
@@ -3,30 +3,34 @@
  * @return {string[]}
  */
 var generateParenthesis = function(n) {
-    // DFS recursive strategy
+    // DFS recursive strategy (without outside variables)
     // use the decision tree strategy outlined by tejavenkat lanka
     // https://leetcode.com/problems/generate-parentheses/description/comments/1574327
 
     // track how many open/closed parentheses we have
     // closed can never be greater than open
 
-    let answer = [];
-
     function recursion(str, open, closed) {
         // base case(s)
         if (closed == n) {
-            answer.push(str);
+            return [str];
         }
 
         // recursive call(s)
+        let openPath = [];
+        let closedPath = [];
         // open parentheses
-        if (open < n) recursion(str+'(', open+1, closed);
+        if (open < n) {
+            openPath = recursion(str+'(', open+1, closed);
+        }
 
         // closed parentheses
-        if (closed < n && closed < open) recursion(str+')', open, closed+1);
+        if (closed < n && closed < open) {
+            closedPath = recursion(str+')', open, closed+1);
+        }
+
+        return [...openPath, ...closedPath];
     }
 
-    recursion('', 0, 0);
-
-    return answer;
+    return recursion('', 0, 0, []);
 };
