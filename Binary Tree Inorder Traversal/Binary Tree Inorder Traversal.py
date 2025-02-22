f372598d236e0c3d73bac52dab73d9f6a71d8a9c
Derek Gomez <demagome@ucsc.edu>
2025-02-21 23:59:25 -0800

    COMMIT Binary Tree Inorder Traversal

---
+++ Binary Tree Inorder Traversal/Binary Tree Inorder Traversal.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        # recursive solution
+
+        # base case
+        if not root: return
+
+        answer = []
+
+        # recursive calls
+        if root.left:
+            answer.extend( self.inorderTraversal(root.left) )
+
+        answer.append(root.val)
+
+        if root.right:
+            answer.extend( self.inorderTraversal(root.right) )
+
+        # return
+        return answer
