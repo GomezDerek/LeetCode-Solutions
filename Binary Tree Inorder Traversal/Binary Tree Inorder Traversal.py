9a4d83f2d51eb5a2db0bd2bc9bbc241c12c07d64
Derek Gomez <demagome@ucsc.edu>
2025-02-22 02:29:23 -0800

    COMMIT Binary Tree Inorder Traversal

--- Binary Tree Inorder Traversal/Binary Tree Inorder Traversal.py
+++ Binary Tree Inorder Traversal/Binary Tree Inorder Traversal.py
@@ -6,19 +6,48 @@
 #         self.right = right
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        # recursive solution
+        # iterative solution
 
-        # base case
-        if not root: return []
+        breadCrumbs = []
+        # answer = []
+        answer = {}
 
-        answer = []
+        traveler = root
 
-        # recursive calls
-        answer.extend( self.inorderTraversal(root.left) )
+        while traveler:
+            # navigate as far left as possible
+            if traveler.left:
+                breadCrumbs.append( traveler )
+                traveler = traveler.left
 
-        answer.append(root.val)
+            # navigate right bc only the right node is available
+            elif traveler.right:
+                # answer.append( traveler.val )
+                answer[ traveler.val ] = True
+                breadCrumbs.append( traveler )
+                traveler = traveler.right
 
-        answer.extend( self.inorderTraversal(root.right) )
+            # we're at the root; with no leaf nodes nor sub-branches
+            elif not breadCrumbs:
+                # answer.append( traveler.val )
+                answer[ traveler.val ] = True
+                traveler = None
 
-        # return
-        return answer
+            # leaf node
+            else:
+                # answer.append( traveler.val ) # add traveler.val to answer
+                answer[ traveler.val ] = True
+
+                traveler = breadCrumbs.pop() # traveler = parent
+
+                # sever leaf node, so we don't revisit it
+                # if traveler.left and traveler.left.val == answer[-1]:
+                if traveler.left and answer[ traveler.left.val ]:
+                    traveler.left = None
+
+                # elif traveler.right and traveler.right.val == answer[-1]:
+                elif traveler.right and answer[ traveler.right.val ]:
+                    traveler.right = None
+
+        # return answer
+        return list(answer.keys())
