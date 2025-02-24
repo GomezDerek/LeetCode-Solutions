19b7fb11f081ee2578a73d489ef1df5ef7b9f4b1
Derek Gomez <demagome@ucsc.edu>
2025-02-23 20:18:17 -0800

    COMMIT Car Fleet

--- Car Fleet/Car Fleet.js
+++ Car Fleet/Car Fleet.js
@@ -1,59 +1,40 @@
+/**
+ * @param {number} target
+ * @param {number[]} position
+ * @param {number[]} speed
+ * @return {number}
+ */
 var carFleet = function(target, position, speed) {
-    // strategy
-    // simulate car movement by iteratively updating positions with respective speed
-    // when positions overlap, combine
     
-    // notes: input arrays are not sorted
-    
-    // use an ascending monotonic stack to track fleet positions
-    // when iterating positions, combine fleets if currFleetPosition < prevFleetPosition
-    
-    const Fleet = function(pos, spd) {
+    // fleet obj
+    const Fleet = function(pos,spd, time) {
         this.pos = pos;
         this.spd = spd;
+        this.time = time;
     }
-    
-    const fleets = position.map( (p, i) => new Fleet(p, speed[i]) ); // fleets's elements store both position & speed of a fleet
-    fleets.sort( (a,b) => a.pos - b.pos ); // sort in ascending by position
-    // console.log(fleets);
 
-    const arrivedFleets = []; 
+    const fleets = [];
 
-    while (fleets.length) { // while not all fleets have arrived yet
-        
-        // update all fleet positions
-        let i=0;
-        while (i<fleets.length) {
+    for (let i=0; i<position.length; i++) {
+        const pos = position[i];
+        const spd = speed[i];
+        const time = (target - pos)/spd;
 
-            // if fleet has already met target, transfer fleets to arrivedFleets
-            if ( fleets[i].pos >= target ) {
-                arrivedFleets.push(fleets[i]);
-                fleets.splice(i,1); 
-                continue;
-            }
+        fleets.push( new Fleet(pos, spd, time) );
+    }
 
-            // update fleet position
-            fleets[i].pos += fleets[i].spd; 
+    fleets.sort( (a,b) => a.pos - b.pos );
 
-            // combine fleets if they overlap (skip 1st element to avoid out of bounds)
-            if ( i>0 && fleets[i-1].pos >= fleets[i].pos ) { 
+    let endingFleets = 0;
 
-                const currFleet = fleets[i]; // obj ref
-
-                // combine fleets
-                const prevFleetSpeed = fleets[i-1].spd;
-                fleets.splice(i-1, 1); // in-place deletion of prev element; O(n)
-                currFleet.spd = Math.min(currFleet.spd, prevFleetSpeed); // "The speed of the car fleet is the minimum speed of any car in the fleet"
-            
-                continue;
-            }
-
-            // only iterate index if neither of the above if statements execute
-            i++;
+    fleets.forEach( (currFleet, i) => {
+        // if fleet will never catch up to next fleet
+        // it will reach the target without merging
+        const nextFleet = fleets[i+1];
+        if ( !nextFleet || currFleet.time > nextFleet.time ) {
+            endingFleets++;
         }
-        // console.log('Updated fleet:');
-        // console.log(fleets);
-    }
-    
-    return arrivedFleets.length;
+    });
+
+    return endingFleets;
 };
