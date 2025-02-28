1ee91c7a05a9c2aaaa687c414a3ef138672c688b
Derek Gomez <demagome@ucsc.edu>
2025-02-28 00:23:16 -0800

    COMMIT Minimum Deletions to Make Array Beautiful

--- Minimum Deletions to Make Array Beautiful/Minimum Deletions to Make Array Beautiful.js
+++ Minimum Deletions to Make Array Beautiful/Minimum Deletions to Make Array Beautiful.js
@@ -3,41 +3,25 @@
  * @return {number}
  */
 var minDeletion = function(nums) {
-    // brute force
-    // iterate and find every ugly i%2 O(n)
-    // for every ugly i%2: fix it and check if it fixes the array O(n^2)
-
-    // test: [1,1,2,2,3,3]
-    // ugly: [4,2,0]
-    // delete 0th ugly from test
     
-    // ! beautiful bc length is odd
-    // test: [1,2,2,3]
-    // ugly: [4,2]
-    // deletions: 1
-
     let deletions = 0;
     let uglyStack = [];
 
     // find all uglies, regardless if i%2==0
-    let i=nums.length-2;
-    while (i>=0) {
+    for (let i=nums.length-2; i>=0; i--) {
         if (nums[i] == nums[i+1]) uglyStack.push(i);
-        i--;
     }
 
-    console.log('initialized',uglyStack);
-
+    // pop through the entire stack
     while (uglyStack.length) {
-        const uglyPos = uglyStack.pop() - deletions;
-        // console.log('uglyPos', uglyPos);
-        if ( uglyPos%2 == 0 ) {
-            nums.splice(uglyPos,1);
-            deletions++;
-            // console.log(uglyPos, ' deleted');
-            // console.log('new nums: ', nums)
-        }
+
+        // subtrgact dels to calc index in hypothetically modded array
+        const uglyInd = uglyStack.pop()-deletions; 
+
+        // "delete" if uglyIndex is even
+        if (uglyInd%2 == 0) deletions++;
     }
 
-    return nums.length%2 == 0 ? deletions : deletions +1;
+    // length must be even to be beautiful
+    return (nums.length - deletions)%2 == 0 ? deletions : deletions +1;
 };
