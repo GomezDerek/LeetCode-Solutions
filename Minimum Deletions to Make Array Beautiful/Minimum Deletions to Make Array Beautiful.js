44beeb138b90b74cac6b9e0a2ace8a5fbb2200ba
Derek Gomez <demagome@ucsc.edu>
2025-02-28 00:36:36 -0800

    COMMIT Minimum Deletions to Make Array Beautiful

--- Minimum Deletions to Make Array Beautiful/Minimum Deletions to Make Array Beautiful.js
+++ Minimum Deletions to Make Array Beautiful/Minimum Deletions to Make Array Beautiful.js
@@ -5,23 +5,10 @@
 var minDeletion = function(nums) {
     
     let deletions = 0;
-    let uglyStack = [];
 
-    // find all uglies, regardless if i%2==0
-    for (let i=nums.length-2; i>=0; i--) {
-        if (nums[i] == nums[i+1]) uglyStack.push(i);
+    for (let i=0; i<nums.length-1; i++) {
+        if ( nums[i-deletions]%2==0 && nums[i] == nums[i+1] ) deletions++;
     }
 
-    // pop through the entire stack
-    while (uglyStack.length) {
-
-        // subtrgact dels to calc index in hypothetically modded array
-        const uglyInd = uglyStack.pop()-deletions; 
-
-        // "delete" if uglyIndex is even
-        if (uglyInd%2 == 0) deletions++;
-    }
-
-    // length must be even to be beautiful
-    return (nums.length - deletions)%2 == 0 ? deletions : deletions +1;
+    return (nums.length-deletions)%2 == 0  ? deletions : deletions +1;
 };
