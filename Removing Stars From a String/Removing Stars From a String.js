04fb9bf2216aba914374687d02ad68d476efa1ad
Derek Gomez <demagome@ucsc.edu>
2025-03-02 00:24:05 -0800

    COMMIT Removing Stars From a String

--- Removing Stars From a String/Removing Stars From a String.js
+++ Removing Stars From a String/Removing Stars From a String.js
@@ -3,12 +3,16 @@
  * @return {string}
  */
 var removeStars = function(s) {
-    let answer = "";
+    const stack = [];
 
     for (let i=0; i<s.length; i++) {
-        if ( s[i] == "*" ) answer = answer.slice(0,answer.length-1);
-        else answer += s[i];
+        if ( s[i] == "*" ) stack.pop();
+        else stack.push( s[i] );
     }
 
+    let answer = "";
+    for (let i=0; i<stack.length; i++) {
+        answer += stack[i];
+    }
     return answer;
 };
