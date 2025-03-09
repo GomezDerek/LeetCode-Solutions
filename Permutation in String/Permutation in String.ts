2ddf0db999216704cb73322ea6c74abc11226edf
Derek Gomez <demagome@ucsc.edu>
2025-03-09 15:36:24 -0700

    COMMIT Permutation in String

--- Permutation in String/Permutation in String.ts
+++ Permutation in String/Permutation in String.ts
@@ -21,14 +21,29 @@ function checkInclusion(s1: string, s2: string): boolean {
 
     let remFreqMap:{[key:string]:number} = {...ogFreqMap};
 
+    let l:number = 0;
     for (let i=0; i<s2.length; i++) {
         const ch = s2[i];
         // console.log(i, remFreqMap);
-        if (remFreqMap[ch] == undefined || remFreqMap[ch] <= 0) {
-            // reset freqMap
+
+        // ch not in s1
+        if (remFreqMap[ch] == undefined) {
+            // reset freqMap & l pointer
             remFreqMap = {...ogFreqMap};
+            l=i;
         }
 
+        // extra ch found
+        else if (remFreqMap[ch] == 0) {
+            // shift l to exclude first ch
+            while(s2[l] != ch) {
+                remFreqMap[s2[l]]++;
+                l++;
+            }
+            l++;
+        }
+
+        // ch is in s1
         else if (remFreqMap[ch] > 0) {
             // decrement
             remFreqMap[ch]--;
@@ -45,46 +60,4 @@ function checkInclusion(s1: string, s2: string): boolean {
     // no permutations found
     return false;
 
-    // get the index value for remFreqArr
-    // function getIndex(ch:string):number {
-    //     return ch.charCodeAt(0) - 'a'.charCodeAt(0);
-    // }
-
-    // create the frequency arrays
-    // const ogFreqArr:number[] = new Array(26).fill(0); // ascii val [a,z]
-    // for (const ch of s1) {ogFreqArr[getIndex(ch)]++};
-
-    // let remFreqArr:number[] = ogFreqArr.slice(0);
-    // console.log(remFreqArr);
-
-    // create the frequency maps
-
-    // for (let r=0; r<s2.length; r++) {
-    //     const ch = s2[r];
-    //     const chI = getIndex(ch);
-
-    //     // ch of s2 not in freqArr
-    //     if (remFreqArr[chI] == undefined) {
-    //         // reset freqArr
-    //         remFreqArr = ogFreqArr.slice(0);
-    //         console.log(`reset at ${r},${ch}`);
-    //         console.log(remFreqArr);
-    //     }
-
-    //     // if freq has reached 0
-    //     else if ( remFreqArr[chI] <= 0 ) {
-    //         console.log(`0 ${ch}`);
-    //         // permutation found?
-    //         if ( remFreqArr.every(freq => freq==0) ) return true;
-    //         else 
-    //     }
-
-    //     // decrement ch freq
-    //     else if (remFreqArr[chI] > 0){
-    //         remFreqArr[chI]--;
-    //     }
-    // }
-
-    // // no permutation found
-    // return false;
 };
