8a98ed08a1819f08f14ff68117cab518d550b4bb
Derek Gomez <demagome@ucsc.edu>
2025-03-09 12:40:53 -0700

    COMMIT Longest Repeating Character Replacement

--- Longest Repeating Character Replacement/Longest Repeating Character Replacement.ts
+++ Longest Repeating Character Replacement/Longest Repeating Character Replacement.ts
@@ -1,77 +1,23 @@
 function characterReplacement(s: string, k: number): number {
-    // strategy
     /* 
-    Step 1: identify longest existing string
-    Step 2: expand string to connect to matching characters
-            a. how to decide whether to expand left or right?
-               - stay within bounds
-               - while k>0
-               - recursive decision tree?
-
-    Questions:
-        can s[i] be more than either A || B ?
-        if s.length==0, answer == ""
-        will s[i] always be uppercase?
+    as we iterate from left -> right
+    record the starting point
+    try to create as long a string as possible with k substitutions
+    O(n^2)
 
     */
 
-    // find longest existing string O(n)
-    let maxCh: string = s[0];
-    let maxRange: number[] = [0,0];
+    let maxLength: number = 1;
 
-    let curCh: string = s[0];
-    let curStart: number = 0;
-
-    for (let i=1; i<s.length; i++) {
-
-        // if this string is longer than our max, update the max
-        if (s[i]==curCh && i-curStart > maxRange[1]-maxRange[0]) {
-            maxCh = s[i];
-            maxRange = [curStart, i];
-        }
-
-        // new ch -> new substring
-        else {
-            curCh = s[i];
-            curStart = i;
+    for (let i=0; i<s.length; i++) {
+        let subsRemaining:number = k;
+        let j=i+1;
+        while (subsRemaining > 0 || s[j] == s[i]) {
+            if (s[j] != s[i]) subsRemaining--;
+            maxLength = Math.max(maxLength, j-i+1);
+            j++;
         }
     }
 
-    // console.log(maxCh, maxRange);
-
-    // expand string to connect to matching characters O(k)
-    function recursion(start, end, k): number[] {
-        // console.log(start,end,k);
-        // base case(s)
-        if (k<=0) return [start, end]
-
-        // recursive call(s)
-        let left:number[] = [null,null];
-        if (start>0 && s[start]==s[end]) { // can expand left w/o substitution
-            // console.log('l -> k');
-            left = recursion(start-1, end, k);
-        }
-        else if (start>0) { // expand left WITH substitution
-            // console.log('l -> k-1');
-            left = recursion(start-1, end, k-1);
-        }
-
-        // console.log(`${s[end]}==${s[end+1]} ? ${s[end]==s[end+1]}`);
-        let right:number[] = [null,null];
-        if (end<s.length-1 && s[end]==s[start]) { // can expand right w/o substitution
-            // console.log('r -> k');
-            right = recursion(start, end+1, k); 
-        }
-        else if (end<s.length-1) { // expand right WITH substitution
-            // console.log('r -> k-1');
-            right = recursion(start, end+1, k-1);
-        }
-
-        // return
-        // console.log([left[0] || start, right[1] || end]);
-        return [left[0] || start, right[1] || end];
-    }
-
-    const finalRange:number[] = recursion(maxRange[0], maxRange[1],k);
-    return finalRange[1] - finalRange[0] +1;
+    return maxLength;
 };
