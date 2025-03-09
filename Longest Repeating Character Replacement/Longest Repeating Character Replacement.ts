023e4a96d1aee261a26865e31e781c84bcc0cbc0
Derek Gomez <demagome@ucsc.edu>
2025-03-09 13:04:17 -0700

    COMMIT Longest Repeating Character Replacement

--- Longest Repeating Character Replacement/Longest Repeating Character Replacement.ts
+++ Longest Repeating Character Replacement/Longest Repeating Character Replacement.ts
@@ -1,21 +1,32 @@
 function characterReplacement(s: string, k: number): number {
     /* 
+    O(n^2) solution
     as we iterate from left -> right
     record the starting point
-    try to create as long a string as possible with k substitutions
-    O(n^2)
-
+    create the longest substring going to the right as possible
+    do the same for the left
     */
 
     let maxLength: number = 1;
 
     for (let i=0; i<s.length; i++) {
         let subsRemaining:number = k;
-        let j=i+1;
+        let j:number = i+1;
+
+        // expand right
         while ( (subsRemaining > 0 || s[j] == s[i]) && j<s.length) {
             if (s[j] != s[i]) subsRemaining--;
             maxLength = Math.max(maxLength, j-i+1);
             j++;
+        }
+
+
+        // expand left
+        let l:number = i-1;
+        while( (subsRemaining > 0 || s[l] == s[i]) && l>=0) {
+            if (s[i] != s[l]) subsRemaining--;
+            maxLength = Math.max(maxLength, j-l);
+            l--;
         }
     }
 
