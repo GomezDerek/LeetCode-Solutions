1d3772b0bcbadd842b7cbf7a4d95e7257144e9de
Derek Gomez <demagome@ucsc.edu>
2025-04-01 11:19:17 -0700

    COMMIT Reorder List

--- Reorder List/Reorder List.js
+++ Reorder List/Reorder List.js
@@ -16,31 +16,30 @@ var reorderList = function(head) {
     array indices track original order
     iterate through array and reassign node.next pointers
 
+
     Runtime:   O(2N) -> O(N)
     Spacetime: O(N)
     */
 
     // create and fill array
     const a = [];
+    let n = 0;
     while (head) {
         a.push(head);
         head = head.next;
+        n++;
     }
 
     // reorder!
-    for (let i=0; i<a.length/2; i++) {
+    for (let i=0; i<n/2; i++) {
         const leftNode = a[i];
-        const rightNode = a[a.length-i-1]; 
+        const rightNode = a[n-i-1]; 
 
+        rightNode.next = leftNode.next;
         leftNode.next = rightNode;
-
-        // if rightNode is not nth node
-        if (i>0) {
-            const nextRightNode = a[a.length-i];
-            nextRightNode.next = leftNode;
-        }
-
-        rightNode.next = null; 
     }
 
+    // new tail will point at null, to avoid cycle
+    // a[ n//2 ] returns the middle node (& new tail) from the original list
+    a[ parseInt(n/2) ].next = null;
 };
