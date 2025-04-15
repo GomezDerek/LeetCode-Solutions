117ded79c189e81e28cd09c024f569ad371fd093
Derek Gomez <demagome@ucsc.edu>
2025-04-15 11:11:45 -0700

    COMMIT Search a 2D Matrix

--- Search a 2D Matrix/Search a 2D Matrix.js
+++ Search a 2D Matrix/Search a 2D Matrix.js
@@ -11,21 +11,23 @@ var searchMatrix = function(matrix, target) {
         this.row = row;
         this.col = col;
         this.val = () => {return matrix[this.row][this.col]};
-        this.dist = () => {return (n)*(this.row) + this.col}
+        this.dist = () => {return (n)*(this.row) + this.col};
+        this.valid = () => {return this.row >= 0 && this.row < m && this.col >= 0 && this.col < n};
     }
 
     let left = new Pointer(0,0);
     let right = new Pointer(m-1, n-1);
     let middle = new Pointer(null,null);
     // middle = new Pointer(right.row, left.col);
-    // console.log(left.val(), right.val());
+    console.log(left.val(), right.val());
     // console.log(left.dist(), middle.dist(), right.dist());
     // return;
 
-    while (left.val() <= right.val()) {
+    while (left.valid() && right.valid() && left.val() <= right.val()) {
         let newMidDist = parseInt((left.dist() + right.dist())/2);
         middle.row = parseInt(newMidDist/n);
         middle.col = newMidDist%n;
+        // console.log(middle.val());
         // middle.row = parseInt((left.row + right.row)/2);
         // middle.col = parseInt((left.col + right.col)/2);
 
