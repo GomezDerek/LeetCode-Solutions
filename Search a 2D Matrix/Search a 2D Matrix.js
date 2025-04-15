8def05125b8fc79c6180366697b8b953710252db
Derek Gomez <demagome@ucsc.edu>
2025-04-15 12:31:03 -0700

    COMMIT Search a 2D Matrix

--- Search a 2D Matrix/Search a 2D Matrix.js
+++ Search a 2D Matrix/Search a 2D Matrix.js
@@ -13,6 +13,10 @@ var searchMatrix = function(matrix, target) {
         this.val = () => {return matrix[this.row][this.col]};
         this.dist = () => {return (n)*(this.row) + this.col};
         this.valid = () => {return this.row >= 0 && this.row < m && this.col >= 0 && this.col < n};
+        this.newDist = (dist) => {
+            this.row = parseInt(dist/n);
+            this.col = dist%n;
+        };
     }
 
     let left = new Pointer(0,0);
@@ -20,28 +24,17 @@ var searchMatrix = function(matrix, target) {
     let middle = new Pointer(null,null);
 
     while (left.valid() && right.valid() && left.val() <= right.val()) {
-        let newMidDist = parseInt((left.dist() + right.dist())/2);
-        middle.row = parseInt(newMidDist/n);
-        middle.col = newMidDist%n;
+        let midDist = parseInt((left.dist() + right.dist())/2);
+        middle.newDist(midDist);
 
         if (middle.val() == target) {
             return true;
         }
         else if (middle.val() < target) { // too far left
-            // we want left to be the cell to the right of middle
-            // if the middle is the last number in the row,
-            // we set left to the first number in the next row
-            let newLeftDistance = newMidDist +1;
-            left.row = parseInt(newLeftDistance/n);
-            left.col = newLeftDistance%n;
+            left.newDist(midDist + 1);
         }
         else if (middle.val() > target) { // too far right
-            // we want right to be the cell to the left of the middle
-            // if the middle is the first number in the row
-            // we set right to the last number in the prev row
-            let newRightDistance = newMidDist -1;
-            right.row = parseInt(newRightDistance/n);
-            right.col = newRightDistance%n;
+            right.newDist(midDist - 1);
         }
     }
 
