b4e1405ee8f3c0952e8a7513d45fe7cc70fc6809
Derek Gomez <63622469+GomezDerek@users.noreply.github.com>
2025-05-07 12:24:28 -0700

    COMMIT Median of Two Sorted Arrays

--- Median of Two Sorted Arrays/Median of Two Sorted Arrays.ts
+++ Median of Two Sorted Arrays/Median of Two Sorted Arrays.ts
@@ -1,115 +1,102 @@
 function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
+    const totalLength: number = nums1.length + nums2.length;
+    const halfLength: number = Math.floor(totalLength/2);
+
+    // calc smaller array
+    let a: number[] = nums1;
+    let b: number[] = nums2;
+
+    // assume a is smaller, and correct if assumption is wrong
+    if (a.length > b.length || !a.length) {        
+        a = nums2;
+        b = nums1;
+    }
+
+    let l: number = -1;
+    let r: number = a.length;
+
+    while (l <= r) {
+        // console.log(l,r)
+        // calc partiton for smaller
+        let mid: number = Math.trunc((l+r)/2);
+
+        // calc partition for bigger
+        let j: number = halfLength - mid - 2;
+
+        // right-most value in left partition in smaller array
+        let aLeft: number = a[mid] != undefined ? a[mid] : -Infinity;
+
+        // left-most value in right partition in smaller array
+        let aRight: number = a[mid+1] != undefined ? a[mid+1] : Infinity;
+
+        // right-most value in left partition in larger array
+        let bLeft: number = b[j] != undefined ? b[j] : -Infinity;
+
+        // left-most value in right partition in larger array
+        let bRight: number = b[j+1] != undefined ? b[j+1] : Infinity;
+
+        // console.log(aLeft, bLeft, aRight, bRight);
+
+        if (aLeft > bRight) {// if invalid
+            // l = mid + 1;
+            r = mid -1;
+        }
+        else if (bLeft > aRight) { // or invalid
+            // r = mid - 1;
+            l = mid + 1;
+        }
+        else { // both valid
+            // calc median
+            if (totalLength % 2 == 0) { // even
+                return (Math.max(aLeft, bLeft) + Math.min(aRight, bRight))/2;
+            }
+            else { // odd
+                console.log(aLeft, bLeft, aRight, bRight);
+                return Math.min(aRight, bRight);
+            }
+        }
+    }
     /*
-    OBSERVATIONS
-    merging the arrays is O(m+n), which is too slow
-    O(log m) + O(log n) = O(log (m+n)) ?
-    No...
-    
-    O(log m) * O(log n) = O(log (m+n))
-    No...
-    
-    O(log m) + O(log n) = O(log mn)
-    Correct...
+    Pseudo-code:
+calc/store total length
+calc/store half length
 
-    O(log (m+n)) means we need to do BSA on the combined length
+calc smaller array
+set left and right pointers to ends of smaller array
 
-    nums1 and nums2 can have overlapping ranges
+start our binary search
+ calc middle/partition for smaller array
+ calc mid/partition for larger array
+ check if our partition is valid
 
-    STRATEGY
-    2 situations:
-    no overlap
-    yes overlap
+ if smaller[middle] > larger[middle+1]
+  else move left to mid + 1
 
-    in the test cases, you can get right answer from finding the avg of each array's median
-    answer = (nums1Median + nums2Median)/2
-    is it this easy...?
+ else if larger[middle] > smaller[middle+1]
+  else move right to mid -1
 
-    nums1 = [10,11]
-    nums2 = [1,2,3]
-    median1 = 10.5;
-    median2 = 2
-    (10.5 + 2)/2 = 6.25
-    WRONG
-
-    refuted...
-
-    finding the median of 1 array is O(1).
-    if array.length %2 == 1, median = array[ FLOOR(length/2) ]
-
-    nums1 = [1,6,100]
-    nums2 = [2,80]
-    median = 6
-
-    Will I need to code a bunch of eddge cases?
-    considering even vs odd lengths, and overlapping vs non-overlapping arrays
-
-    if non-overlapping
-    [nums1] + [nums2]
-    median = (nums1.length + nums2.length)/2
-
-    if overlapping...
-    [ min(nums1[0], nums2[0]) ... max(nums1[-1], nums2[-1]]) ]
-
-    how to confirm a median in an overlapping array?
-    values < median == values > median
-    THIS ☝️
-
-    1st median = (min+max)/2
-    lesserCount = 0
-    greaterCount = 0
-    l = min
-    r = max
-    while(lesserCount != greaterCount) {
-        proposedMedian = (l+r)/2
-        lesser1, greater1 = findPlaceInArray(proposed, arr1)
-        lesser2, greater2 = findPlaceInArray(proposed, arr2)
-        lesserCount = lesser1 + lesser2;
-        greaterCount = greater1 + greater2
-
-        if lesserCount > greaterCount {
-            move right pointer
-        }
-        else if greaterCount > lesserCount {
-            move left pointer
-        }
-    }
-    // exit when we have equality
-    return proposedMedian
+ else partition is valid
+  calculate the median
+  if odd - then return min(smaller+1, larger+1)
+  else even - then return (max(smaller or larger middle) + min(smaller+1, larger+1))/2
 
 
-    findPlaceInArray(val, arr) {
-        lesser, greater
-        //BSA
-        l,r,m
-        while(l <= r) {
-            if arr[m] == val {
-                lesser = m
-                greater = length - lesser - 1;
-            }
-            if arr[m] < m < arr[m+1] {
-                lesser = m + 1
-                greater = length - lesser
-            }
-            else if m is too low
-                move left pointer
-
-            else i fm is too high
-                move right pointer
-        }
-    }
-    [1,1,2,3,3,5,7]
-     0 1 2 3 4 5 6
-    [1,3,5,7]
-    [1,2,3]
-     0 1 2
-     len = 3
-     m = 1
-     lesser = 1
-     greater = 1
 
 
-    RESTART AT 2 HOURS
-    just deleting everything and trying NC's solution after watching his explanation, but not code
-    */
-    
+
+  if smaller[middle] <= larger[middle+1] && larger[middle] <= smaller[middle+1]
+  calculate the median
+        *consider out of bounds accesses
+
+
+
+if(smaller[middle]> larger[middle+1]{
+move left to the right mid +1
+else {
+move right to the left mid -1
+
+
+
+
+    */    
 };
