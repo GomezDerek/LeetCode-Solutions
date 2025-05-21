e70f9b14a689b64369672985569b43e9a0d0279a
Derek Gomez <63622469+GomezDerek@users.noreply.github.com>
2025-05-21 11:30:22 -0700

    COMMIT Lowest Common Ancestor of a Binary Search Tree

--- Lowest Common Ancestor of a Binary Search Tree/Lowest Common Ancestor of a Binary Search Tree.ts
+++ Lowest Common Ancestor of a Binary Search Tree/Lowest Common Ancestor of a Binary Search Tree.ts
@@ -12,79 +12,38 @@
  * }
  */
 
+// solution based off of Neetcode's hints
+
 /*
+ASSUME: 
+    tree is balanced and sorted
 
-GOAL:
-    given 2 nodes, find their LCA in the tree
-
-
-ASSUME:
-    trees are balanced
+NOTES:
+    our LCA is the root node of p & q's shared subtree
+    subtrees split after the LCA
+    p.val <= LCA <= q.val
 
 STRATEGY:
-    O(n)
-    DFS to find each node
-    as we traverse to find p & q,
-        record their traversal paths (node values traversed)
-
-    O( log n * log n) -> O( log 2n ) 
-    after they've been found,
-    find their LCA by iterating thorugh their traversal paths
-    LCA is last shared value
-
-EDGE CASES:
-    at least 2 nodes
-    p != q
-    p and q always exist in BST
+    traverse the tree using the p,q values to pick paths
+    when we need to split, return the LCA
 */
 
 function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
+    // edge case: p < q == false
     
-    const paths: { [key: number]: TreeNode[] } = {
-        [p.val]: [],
-        [q.val]: []
-    }
-
-    findPath(root, p.val, []);
-    findPath(root, q.val, []);
-
-    // console.log(paths);
-    
-    // iterate backwards through p path
-    for (let i=paths[p.val].length-1; i >= 0; i--) {
-        // iterate backwards through q path
-        // console.log(paths[p.val][i]);
-        for (let j=paths[q.val].length-1; j >= 0; j--) {
-            // console.log(paths[p.val][i], paths[q.val][j]);
-            // console.log(typeof paths[p.val][i], typeof paths[q.val][j]);
-            if (paths[p.val][i].val === paths[q.val][j].val) return paths[p.val][i];
-        }
-    }
-
-    console.log("LCA never found!");
-    return null;
-
-    // helper functions
-    // updates global path variables
-    function findPath(node: TreeNode, targetVal: number, path: TreeNode[]): void {
-        // base case
-        if (node === null) return;
-
-        // add current node to path
-        path.push(node);
+    while (root !== null) {
+        // p or q is the LCA
+        if (root.val == p.val || root.val == q.val) return root;
         
-        // target found, update its global path variable
-        if (node.val === targetVal) {
-            paths[targetVal] = path;
-            return;
-        }
-        
-        // recursion
-        // continue searching for the target node
+        // traverse left
+        else if (root.val > p.val && root.val > q.val) root = root.left;
+
+        // traverse right
+        else if (root.val < p.val && root.val < q.val) root = root.right;
+
+        // we found the split node/LCA
         else {
-            findPath(node.left, targetVal, [...path]);
-            findPath(node.right, targetVal, [...path]);
-            return;
+            return root;
         }
     }
 };
