13b68db16be29b2433696b8c733eabca041dc0eb
Derek Gomez <63622469+GomezDerek@users.noreply.github.com>
2025-07-01 15:09:30 -0700

    COMMIT Longest Increasing Subsequence

--- Longest Increasing Subsequence/Longest Increasing Subsequence.ts
+++ Longest Increasing Subsequence/Longest Increasing Subsequence.ts
@@ -4,41 +4,35 @@ this is how I remember his brute force, recursive solution O(2^n)
 
 STRATEGY:
 the decision tree starts at index 0, and is as follows:
-    attempt to add the next index to the current subsequence
-    and
-    start a new subsequence at the next index
-
+    measure the length of the current subsequence
+    attempt to add every remaining value to the subsequence
+    and 
+    start a new subsequence at the next value
 */
 
 function lengthOfLIS(nums: number[]): number {
     
-    let longest: number = 1;
-    recurse([nums[0]], 1);
+    let longest: number = -1;
+    recurse([nums[0]], 0);
     return longest;
 
     // func def
-    function recurse(subSeq: number[], nextI: number): void {
+    function recurse(subSeq: number[], curI: number): void {
         // BASE CASE
-        // nextI is out of bounds
-        if (nextI >= nums.length) return;
 
         // OPERATIONS
-        // console.log(subSeq, nums[nextI]);
-        // if next val is larger, add to the subsequence
-        if ( nums[nextI] > subSeq[subSeq.length-1] ) {
-            subSeq.push(nums[nextI]);
-            // console.log('✅\n');
-        }
-        // else console.log('❌\n');
         longest = Math.max(longest, subSeq.length);
 
-        // RECURSION
-        // recurse(subSeq, nextI+1);
-        // recurse(nums[nextI], nextI+1);
+        // RECURSE
+        // start fresh subseq at next val
+        if (curI < nums.length-2) recurse([nums[curI+1]], curI+1); 
         
-        for (let j=nextI+1; j<nums.length; j++) {
-            recurse([nums[nextI]], j);
-            recurse([...subSeq], j);
+        for (let j=curI+1; j<nums.length; j++) {
+            // try to add all other values to our subseq
+            // BASE CASE?
+            if ( nums[j] > subSeq[subSeq.length-1] ) {
+                recurse( [...subSeq, nums[j]], j );
+            }
         }
     }
 };
