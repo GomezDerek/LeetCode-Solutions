975f28f77096b2290a8ed4472de49409b6e59e03
Derek Gomez <63622469+GomezDerek@users.noreply.github.com>
2025-07-06 04:51:22 -0700

    COMMIT Fibonacci Number

---
+++ Fibonacci Number/Fibonacci Number.ts
@@ -0,0 +1,7 @@
+// naive implementation without cache optimization
+
+function fib(n: number): number {
+    if (n === 0) return 0;
+    else if (n === 1) return 1;
+    else return fib(n-1) + fib(n-2);
+};
