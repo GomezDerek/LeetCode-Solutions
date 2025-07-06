29105ab68d04ce3caff22a3e00e3a2b243fee06c
GitHub Action <action@github.com>
2025-07-06 12:16:26 +0000

    chore: move Fibonacci Number to solutions/

--- solutions/Fibonacci Number/Fibonacci Number.ts
+++ solutions/Fibonacci Number/Fibonacci Number.ts
@@ -1,13 +1,14 @@
+// iterative bottom-up DP
 // optimized runtime with cache
 // Runtime: O(N)
 // Space: O(N)
 
 function fib(n: number): number {
     const cache: number[] = [0,1];
-    return recurse(n);
     
-    function recurse(n: number): number {
-        if (cache[n] === undefined) cache[n] = fib(n-1) + fib(n-2);
-        return cache[n];
+    for (let i=2; i<=n; i++) {
+        cache.push( cache[i-1] + cache[i-2] );
     }
+
+    return cache[n];
 };
