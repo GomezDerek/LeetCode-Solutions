ceb9fe516e15e9e52e87cb479a0f24b471b8facc
GitHub Action <action@github.com>
2025-07-11 09:05:20 +0000

    chore: move Kth Largest Element in a Stream to solutions/

--- solutions/Kth Largest Element in a Stream/Kth Largest Element in a Stream.ts
+++ solutions/Kth Largest Element in a Stream/Kth Largest Element in a Stream.ts
@@ -1,37 +1,105 @@
-// import {
-//   PriorityQueue,
-//   MinPriorityQueue,
-//   MaxPriorityQueue,
-//   ICompare,
-//   IGetCompareValue,
-// } from '@datastructures-js/priority-queue';
-
+/*
+NOTES:
+    parent's index = childIndex // 2
+    lChild index = 2 * parentIndex
+    rChild index = 1 + 2*parentIndex
+*/
 
 class KthLargest {
     k: number;
-    kHeap: MinPriorityQueue<number>;
-    
+    kHeap: number[]; // minHeap of size K
+    kthLargest(): number { return this.kHeap[1] };
+
     constructor(k: number, nums: number[]) {
-        this.k = k; 
-        this.kHeap = MinPriorityQueue.fromArray<number>(nums);
-        
-        // pop until heap is k-sized
-        while (this.kHeap.size() > k) this.kHeap.pop();
+        this.k = k;
+
+        // sort and splice to only include top k vals
+        nums.sort((a,b) => b-a); // O(n logn)
+        nums.splice(k);          // O(n-k)
+
+        this.kHeap = [null, ...nums];
+
+        // heapify - convert to minHeap O(k logk)
+        for (let i=Math.floor(k/2); i>0; i--) {
+            this.bubbleDown(i);
+        }
+        console.log(this.kHeap);
+        // for (let i=k; i>0; i--) {
+
+        //     let j: number = i;
+        //     while ( 
+        //         Math.floor(j/2) > 0 
+        //         && this.kHeap[j] < this.kHeap[ Math.floor(j/2) ] 
+        //     ) {
+        //         // swap with parent
+        //         const tmp: number = this.kHeap[j];
+        //         this.kHeap[j] = this.kHeap[ Math.floor(j/2) ];
+        //         this.kHeap[ Math.floor(j/2) ] = tmp;
+
+        //         j = Math.floor(j/2); // continue bubbling up
+        //     }
+        // }
     }
 
     add(val: number): number {
-        // just add to heap if it's not k-sized yet
-        if (this.k > this.kHeap.size()) this.kHeap.enqueue(val);
-        
-        // replace kth largest
-        else if ( val > this.kHeap.front() ) {
-            this.kHeap.pop();
-            this.kHeap.enqueue(val);
+        if (this.kHeap.length <= this.k) this.kHeap.push( this.kHeap[1] ); // add to our heap
+        else if (val < this.kthLargest()) return this.kthLargest(); // no mods needed
+
+        // else we need to adjust our heap
+        this.kHeap[1] = val; // replace top of min heap
+
+        // reheapify
+        // and bubble up our new val to its rightful place
+        this.bubbleDown(1);
+
+        return this.kthLargest();
+    }
+
+
+    // swap a value up the heap tree from leaf -> root
+    bubbleUp(curI: number): void {
+        while ( 
+            Math.floor(curI/2) > 0 
+            && this.kHeap[curI] < this.kHeap[ Math.floor(curI/2) ] 
+        ) {
+            // swap with parent
+            const tmp: number = this.kHeap[curI];
+            this.kHeap[curI] = this.kHeap[ Math.floor(curI/2) ];
+            this.kHeap[ Math.floor(curI/2) ] = tmp;
+
+            curI = Math.floor(curI/2); // continue bubbling up
         }
+    }
 
-        // else (val < this.kHeap.front()) continue
+    // swap a value down the heap tree from root -> leaf
+    bubbleDown(curI: number): void {
+        while ( curI * 2 < this.kHeap.length) {
+            const lVal: number = this.kHeap[curI*2];
+            const rVal: number = this.kHeap[curI*2 +1] ?? Infinity;
+            const min: number = Math.min(this.kHeap[curI], lVal, rVal);
 
-        return this.kHeap.front();
+            let breakFlag: boolean = false;
+            switch (min) {
+                case this.kHeap[curI]:
+                    breakFlag = true;
+                    break;
+
+                case lVal:
+                    // swap cur with lChild
+                    [this.kHeap[curI], this.kHeap[curI*2]] = [this.kHeap[curI*2], this.kHeap[curI]];
+                    curI *= 2;
+                    break;
+                    
+                case rVal:
+                    // swap cur with rChild
+                    [this.kHeap[curI], this.kHeap[curI*2 +1]] = [this.kHeap[curI*2 +1], this.kHeap[curI]];
+                    curI = curI*2 +1;
+                    break;
+            }
+
+            if (breakFlag) break;
+            // else continue
+        }
     }
 }
 
