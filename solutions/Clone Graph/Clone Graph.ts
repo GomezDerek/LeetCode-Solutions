f1db1fbf5fb42d36adc7a2bccdf00b5e24a1b211
GitHub Action <action@github.com>
2025-07-12 23:01:39 +0000

    chore: move Clone Graph to solutions/

--- solutions/Clone Graph/Clone Graph.ts
+++ solutions/Clone Graph/Clone Graph.ts
@@ -12,59 +12,25 @@
  * 
  */
 
- /*
-    given an adjacency list, create a deep copy of the graph
-    return an adjacency list
-
-    NOTE:
-        beware! assign clone nodes, not refs
-        _Node.neighbors = _Node[]
-
-        we do NOT know exact graph size...
-
-    STRATEGY: O(N)
-        create an empty adjacency list of input size
-        traverse graph with DFS:
-            if clone exists, node alr traversed - return clone
-            else clone DNE 👇
-                create + add clone of cur node
-
-                for all neighbors:
-                    DFS traverse
-                    store neighbor clones
-
-                return cur node clone   
-
-
-        DS:
-        cloneHash: {nodeVal:  neighbors[] }
-        👆auto sorts in asc order by key 
-        O(N) to convert to adjacency list
-        assume node values [1, N]
- */
-
-
+/* DFS strategy */
 function cloneGraph(node: _Node | null): _Node | null {
-    if (node === null) return null; // edge case
-	const cloneMap: {[key: number]: _Node } = {};
+    if (node === null) return null;
+
+    const cloneMap: _Node[] = [];
     dfs(node);
-    return cloneMap[node.val];
+    return cloneMap[1];
 
     function dfs(curNode: _Node): _Node {
-        // base case
-        if (cloneMap[curNode.val] !== undefined) {
-            return cloneMap[curNode.val];
-        }
-        
-        const curClone: _Node = new _Node(curNode.val);
-        cloneMap[curClone.val] = curClone;
+        if ( cloneMap[curNode.val] !== undefined ) return cloneMap[curNode.val];
+
+        const clone: _Node = new _Node(curNode.val);
+        cloneMap[clone.val] = clone;
 
         // recursion
-        curClone.neighbors = curNode.neighbors.map( (adjNode) => {
-            return dfs(adjNode);
-        });
+        for (let i=0; i<curNode.neighbors.length; i++) {
+            clone.neighbors.push( dfs(curNode.neighbors[i]) );
+        }
 
-        // return
-        return curClone;
+        return clone;
     }
 };
