e419d294787f0c1a38f3a1ffb98ce224de306bce
Derek Gomez <63622469+GomezDerek@users.noreply.github.com>
2025-07-13 13:42:20 -0700

    COMMIT Course Schedule

---
+++ Course Schedule/Course Schedule.ts
@@ -0,0 +1,31 @@
+/*
+    GOAL: find a cycle in the DAG
+
+    NOTES: 
+        prerequisites.length up to 5k
+        courses up to 2k
+        cycle exists if backward edge is detected. 
+            back edge = node connects back to existing graph
+
+        ASSUME cycle exists only if classes are direct prereqs of each other
+
+    STRATEGY:
+        create graph from prereqs[]
+        hashmap {node: prereqs}
+        if node exists as a prereq w/in its prereqs' prereqs, cycle exists
+*/
+function canFinish(numCourses: number, prerequisites: number[][]): boolean {
+    const hashMap: {[key: number]: number[]} = {};
+    const courses: number[][] = [];
+    for (let i=0; i<numCourses; i++) courses.push([]);
+
+    for (let i=0; i<prerequisites.length; i++) {
+        const course: number = prerequisites[i][0];
+        const prereq: number = prerequisites[i][1];
+
+        if (courses[prereq].includes(course)) return false;
+        courses[course].push(prereq);
+    }
+
+    return true;
+};
