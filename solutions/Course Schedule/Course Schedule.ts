8a61b5d34b7e5ae3f34916a83a28baa758969722
GitHub Action <action@github.com>
2025-07-14 02:15:24 +0000

    chore: move Course Schedule to solutions/

--- solutions/Course Schedule/Course Schedule.ts
+++ solutions/Course Schedule/Course Schedule.ts
@@ -1,23 +1,10 @@
-/*
-    GOAL: find a cycle in the DAG
+// CONCEDED AT 1 h 15 m
+// then watched NC's solution
+// POST-MORTEM: REMEMBER HOW TO CREATE AN EMPTY 2D ARRAY in TS AHHHHHH
 
-    NOTES: 
-        prerequisites.length up to 5k
-        courses up to 2k
-        cycle exists if backward edge is detected. 
-            back edge = node connects back to existing graph
-
-        ASSUME cycle exists only if classes are direct prereqs of each other
-        👆 Incorrect assumption
-
-    STRATEGY:
-        create graph from prereqs[]
-        hashmap {node: prereqs}
-        if node exists as a prereq w/in its prereqs' prereqs, cycle exists
-*/
 function canFinish(numCourses: number, prerequisites: number[][]): boolean {
     
-    // create adj list
+    // create the adj list
     const courses: number[][] = [];
     for (let i=0; i<numCourses; i++) courses.push([]);
 
@@ -25,42 +12,38 @@ function canFinish(numCourses: number, prerequisites: number[][]): boolean {
         const course: number = prerequisites[i][0];
         const prereq: number = prerequisites[i][1];
 
-        if (course === prereq) return false; // edge case
-        
         courses[course].push(prereq);
     }
 
-    // console.log(courses);
-
-    // look for cycle
-    // returns [vertex, cycleFound]
-    const traversed: boolean[] = new Array(numCourses).fill(false);
+    // look for a cycle, starting from every course
+    const traversed: Set<number> = new Set<number>();
     for (let i=0; i<numCourses; i++) {
-        if ( !traversed[i] && courses[i].length>0) { // if untraversed & has prereqs
-            // if cycle detected
-            if (dfs(i)) return false;
-        };
+        if ( hasCycle(i) ) return false; // cycle detected!
     }
+ 
+    // no cycles detected! 😃
+    return true;
 
-    function dfs(vertex: number): boolean {
+    // func def for dfs traversal
+    function hasCycle(course: number): boolean {
         // base case
-        if (traversed[vertex]) return true;
+        if (traversed.has(course)) return true; // cycle detected  
 
-        traversed[vertex] = true;
+        traversed.add(course);
 
         // recursion
-        // traverse through the prereqs
-        let cycleDetected: boolean = false;
-        for (let i=0; i<courses[vertex].length; i++) {
-            if (dfs(courses[vertex][i])) {
-                cycleDetected = true;
-                break;
-            }
+        const prereqs: number[] = courses[course];
+        for (let i=0; i<prereqs.length; i++) {
+            if (hasCycle(prereqs[i])) return true; // cycle detected!
         }
 
-        // return
-        return cycleDetected;
-    }
+        // reset the set for the next DFS branch
+        traversed.delete(course); 
+        
+        // this course's prereqs are validated. No need to check again in the future
+        courses[course] = []; 
 
-    return true;
+        // cycle not detected
+        return false;
+    }
 };
