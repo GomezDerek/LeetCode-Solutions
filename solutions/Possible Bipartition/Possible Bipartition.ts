5e05c0b28c4081042952e56c9a74276db327b438
GitHub Action <action@github.com>
2025-07-14 07:36:58 +0000

    chore: move Possible Bipartition to solutions/

--- solutions/Possible Bipartition/Possible Bipartition.ts
+++ solutions/Possible Bipartition/Possible Bipartition.ts
@@ -46,23 +46,48 @@ function possibleBipartition(n: number, dislikes: number[][]): boolean {
     console.log(adjList);
 
     // look for fail case
+    const team1: Set<number> = new Set<number>([1]);
+    const team2: Set<number> = new Set<number>();
+
     for (let i=1; i<=n; i++) {
         if ( !bfs(i) ) return false;
     }
     return true; // no fail case found! :D
 
+    type Person = [number, number]; // [person, team]
+
     function bfs(start: number): boolean {
         const visited: Set<number> = new Set();
-        const dq: Deque<number> = new Deque<number>([start]);
+        const dq: Deque<Person> = new Deque<Person>();
+        
+        if ( start === 1) dq.pushFront([1,1]);
+        else if ( team1.has(start) ) dq.pushFront([start,1]);
+        else if ( team2.has(start) ) dq.pushFront([start,2]);
+        else console.error(`${start} has no team at BFS start!`);
 
         while (dq.size() > 0) {
-            const cur: number = dq.popFront();
-            if ( visited.has(cur) ) return false; // fail case
-            // else continue
-            visited.add(cur);
-            
-            console.log(adjList[cur]);
-            adjList[cur].forEach( enemy => dq.pushBack(enemy) );
+            // const cur: number = dq.popFront();
+            const [cur, curTeam] = dq.popFront();
+
+            switch (curTeam) {
+                case 1:
+                    if (team2.has(cur)) return false; // conflict!
+                    else team1.add(cur);
+                    break;
+                case 2:
+                    if (team1.has(cur)) return false; // conflict!
+                    else team2.add(cur);
+                    break;
+            }
+
+            if (visited.has(cur)) continue;
+
+            // traverse to enemies
+            const enemyTeam: number = curTeam === 1 ? 2: 1;
+            adjList[cur].forEach(enemy => {
+                dq.pushBack([enemy, enemyTeam])
+            })
+
         }
 
         return true; // no fail case found
