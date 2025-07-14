265d1dab7e775a9268e851dc227bfe10f9724db5
GitHub Action <action@github.com>
2025-07-14 08:23:03 +0000

    chore: move Possible Bipartition to solutions/

--- solutions/Possible Bipartition/Possible Bipartition.ts
+++ solutions/Possible Bipartition/Possible Bipartition.ts
@@ -48,48 +48,47 @@ function possibleBipartition(n: number, dislikes: number[][]): boolean {
     // look for fail case
     const team1: Set<number> = new Set<number>([1]);
     const team2: Set<number> = new Set<number>();
+    const visiting: Set<number> = new Set<number>();
 
-    for (let i=1; i<=n; i++) {
-        if ( !bfs(i) ) return false;
+    if (!dfs(1, team1)) return false;
+    for (let i=2; i<=n; i++) {
+        if ( !dfs(i, null) ) return false;
     }
+
+    // console.log(visiting);
+    // console.log(team1);
+    // console.log(team2);
     return true; // no fail case found! :D
 
-    type Person = [number, number]; // [person, team]
 
-    function bfs(start: number): boolean {
-        const visited: Set<number> = new Set();
-        const dq: Deque<Person> = new Deque<Person>();
-        
-        if ( start === 1) dq.pushFront([1,1]);
-        else if ( team1.has(start) ) dq.pushFront([start,1]);
-        else if ( team2.has(start) ) dq.pushFront([start,2]);
-        else console.error(`${start} has no team at BFS start!`);
+    function dfs(person: number, team: Set<number>): boolean {
+        // base case
+        if (
+            team !== null &&
+            (team === team1 ? team2: team1).has(person)
+        ) {
+            // console.log(person, " is in the wrong team!");
+            return false;
+        }
+        else if (visiting.has(person)) return true;
 
-        while (dq.size() > 0) {
-            // const cur: number = dq.popFront();
-            const [cur, curTeam] = dq.popFront();
-
-            switch (curTeam) {
-                case 1:
-                    if (team2.has(cur)) return false; // conflict!
-                    else team1.add(cur);
-                    break;
-                case 2:
-                    if (team1.has(cur)) return false; // conflict!
-                    else team2.add(cur);
-                    break;
+        team.add(person);
+        visiting.add(person);
+        // recursion
+        for (let i=0; i<adjList[person].length; i++) {
+            const enemyTeam: Set<number> = team === team1 ? team2: team1;
+            if ( !dfs(adjList[person][i], enemyTeam) ) {
+                // console.log(`parent triggered`);
+                return false;
             }
-
-            if (visited.has(cur)) continue;
-
-            // traverse to enemies
-            const enemyTeam: number = curTeam === 1 ? 2: 1;
-            adjList[cur].forEach(enemy => {
-                dq.pushBack([enemy, enemyTeam])
-            })
-
         }
 
-        return true; // no fail case found
+        return true; // no fail cases found
     }
+
+
 };
+
+// 1: 1
+// 2: 2 
+// 3: 1
