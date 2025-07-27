0346398f4aed39660e7784246cd20f03b685dd10
GitHub Action <action@github.com>
2025-07-27 20:47:28 +0000

    chore: move Valid Anagram to solutions/

--- solutions/Valid Anagram/Valid Anagram.ts
+++ solutions/Valid Anagram/Valid Anagram.ts
@@ -1,15 +1,6 @@
-/**
-    STRATEGY:
-        create frequency lists for each word's letters
-        if any frequency doesn't match,
-            return false, they're not anagrams
- */
-
 function isAnagram(s: string, t: string): boolean {
     if (s.length !== t.length) return false; // can't be anagrams if not same length
 
-    // create freq list for s
-    // use unicode for indices
     const sFreq: number[] = new Array(26).fill(0);
     for (let i=0; i<s.length; i++) {
         sFreq[ s[i].charCodeAt(0) - "a".charCodeAt(0) ]++;
@@ -19,9 +10,8 @@ function isAnagram(s: string, t: string): boolean {
         sFreq[ t[i].charCodeAt(0) - "a".charCodeAt(0) ]--;
     }
 
-    // all freqs should be 0
-    for (const freq of sFreq) {
-        if (freq > 0) return false;
+    for (let i=0; i<26; i++) {
+        if ( sFreq[i] > 0 ) return false;
     }
 
     return true;
