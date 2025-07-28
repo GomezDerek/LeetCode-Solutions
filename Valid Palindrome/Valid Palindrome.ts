cc0dcf67d38805123bc826697f10878ebadb2d6b
Derek Gomez <63622469+GomezDerek@users.noreply.github.com>
2025-07-27 22:22:21 -0700

    COMMIT Valid Palindrome

---
+++ Valid Palindrome/Valid Palindrome.ts
@@ -0,0 +1,31 @@
+function isPalindrome(s: string): boolean {
+    // sanitize the string and store in an array
+    const alphaNums: string[] = [];
+
+    for (const ch of s) {
+        if (isAlphaNum(ch.toLowerCase())) alphaNums.push(ch.toLowerCase());
+    }
+
+    // check if it's a palindrome!
+    for (let i=0; i<alphaNums.length/2; i++) {
+        const lVal: string = alphaNums[i];
+        const rVal: string = alphaNums[alphaNums.length-1-i];
+        if (lVal !== rVal) return false;
+    }
+    
+    // string has survived all palindrome checks
+    return true;
+
+
+    // HELPER FUNCTION
+    function isAlphaNum(ch: string): boolean {
+        // is num
+        if ( Number.isNaN(ch) ) return true;
+
+        // is alpha
+        if ( "a".charCodeAt(0) <= ch.charCodeAt(0) && ch.charCodeAt(0) <= "z".charCodeAt(0)) return true;
+
+        return false;
+    }
+};
+
