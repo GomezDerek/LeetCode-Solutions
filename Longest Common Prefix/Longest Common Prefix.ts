04f7bf5c76e941e55e4a50ca8ca7f871c6ee815a
Derek Gomez <63622469+GomezDerek@users.noreply.github.com>
2025-07-28 08:30:32 -0700

    COMMIT Longest Common Prefix

---
+++ Longest Common Prefix/Longest Common Prefix.ts
@@ -0,0 +1,30 @@
+/**
+    BRUTE FORCE:
+        iterate through every str simultaneously
+            until mismatch
+
+        runtime: O(shortest str * # of str)
+        memory:  O(1)
+ */
+
+function longestCommonPrefix(strs: string[]): string {
+    if (strs.length === 1) return strs[0]; // edge case
+
+    let i=0;
+
+    let done = false;
+    while (i<strs[0].length-1 && !done) {
+        const ch: string = strs[0][i];
+
+        for (const str of strs) {
+            if (ch !== str[i]) {
+                done = true;
+                break;
+            }
+        }
+
+        if (!done) i++;
+    } 
+
+    return strs[0].slice(0,i);
+};
