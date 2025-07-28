8bfd904274e42ed11a7b90df24acbf602dd15b08
GitHub Action <action@github.com>
2025-07-28 05:28:21 +0000

    chore: move Valid Palindrome to solutions/

--- solutions/Valid Palindrome/Valid Palindrome.ts
+++ solutions/Valid Palindrome/Valid Palindrome.ts
@@ -7,7 +7,7 @@ function isPalindrome(s: string): boolean {
     }
 
     // check if it's a palindrome!
-    for (let i=0; i<alphaNums.length/2; i++) {
+    for (let i=0; i<=alphaNums.length/2; i++) {
         const lVal: string = alphaNums[i];
         const rVal: string = alphaNums[alphaNums.length-1-i];
         if (lVal !== rVal) return false;
@@ -20,7 +20,7 @@ function isPalindrome(s: string): boolean {
     // HELPER FUNCTION
     function isAlphaNum(ch: string): boolean {
         // is num
-        if ( Number.isNaN(ch) ) return true;
+        if ( "0".charCodeAt(0) <= ch.charCodeAt(0) && ch.charCodeAt(0) <= "9".charCodeAt(0) ) return true;
 
         // is alpha
         if ( "a".charCodeAt(0) <= ch.charCodeAt(0) && ch.charCodeAt(0) <= "z".charCodeAt(0)) return true;
