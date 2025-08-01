32bd3e3c369752729b47312bb8b958f0a5cc2d3d
GitHub Action <action@github.com>
2025-08-01 08:22:34 +0000

    chore: move Add Two Numbers to solutions/

--- solutions/Add Two Numbers/Add Two Numbers.ts
+++ solutions/Add Two Numbers/Add Two Numbers.ts
@@ -23,34 +23,27 @@
 
 function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
     // build l1's number
-    // first convert from LL -> arr -> str
-    const a1: string[] = [];
+    // first convert from LL -> str
+    let s1: string = "";
     while (l1 != null) {
-        a1.push(l1.val.toString());
+        s1 = l1.val.toString() + s1;
         l1 = l1.next;
-    }
-    a1.reverse();
-    const s1: string = a1.join(""); 
+    } 
     const n1: bigint = BigInt(s1);
-    // console.log(n1);
 
     // do the same to l2
-    const a2: string[] = [];
+    let s2: string = "";
     while (l2 != null) {
-        a2.push(l2.val.toString());
+        s2 = l2.val.toString() + s2;
         l2 = l2.next;
     }
-    a2.reverse();
-    const s2: string = a2.join(""); 
     const n2: bigint = BigInt(s2);
-    // console.log(n2);
 
     // add the numbers!
     const nSum: bigint = n1+n2;
 
     // convert sum to list
     const sSum: string = nSum.toString();
-    // console.log(sSum);
     const sumHat = new ListNode();
     let sumNode = sumHat;
     for (let i=sSum.length-1; i>=0; i--) {
