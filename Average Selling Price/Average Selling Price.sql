33f67f5d4e8521d172f706918b8913c8d9804633
Derek Gomez <63622469+GomezDerek@users.noreply.github.com>
2025-09-07 16:18:43 -0700

    ADD Average Selling Price

---
+++ Average Selling Price/Average Selling Price.sql
@@ -0,0 +1,13 @@
+/* Write your T-SQL query statement below */
+SELECT Prices.product_id
+        ,ROUND(
+            SUM( COALESCE(Sales.units,0.0) * Prices.price ) 
+            / 
+            COALESCE(SUM(Sales.units),1.0) 
+        ,2) AS average_price
+FROM Prices
+LEFT JOIN UnitsSold Sales ON Sales.product_id = Prices.product_id
+WHERE Sales.purchase_date BETWEEN Prices.start_date AND Prices.end_date
+        OR Sales.purchase_date IS NULL
+GROUP BY Prices.product_id
+;
