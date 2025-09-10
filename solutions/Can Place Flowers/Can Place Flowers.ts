2e0ed34313b1c2aab298f4bee7e7281fcdea3a7f
GitHub Action <action@github.com>
2025-09-10 04:30:08 +0000

    chore: move Can Place Flowers to solutions/

--- solutions/Can Place Flowers/Can Place Flowers.ts
+++ solutions/Can Place Flowers/Can Place Flowers.ts
@@ -1,13 +1,8 @@
-/*
-    GOAL: determine whether n new flowers can be planted
-    STRATEGY:
-        simulate with linear iteration and counter
-*/
 function canPlaceFlowers(flowerbed: number[], n: number): boolean {
     let count: number = 0;
 
-    for (let i=0; i<flowerbed.length; i++) {
-        // if left, current, and right plot are empty
+    let i: number = 0;
+    while (i<flowerbed.length) {
         if (
             flowerbed[i] === 0
             && (i===0 || flowerbed[i-1] === 0)
@@ -18,6 +13,7 @@ function canPlaceFlowers(flowerbed: number[], n: number): boolean {
             i++; // skip next
         }
 
+        i++;
     }
 
     return count >= n;
