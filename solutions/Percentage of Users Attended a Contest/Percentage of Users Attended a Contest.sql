60e2b3904382f1ee3bf64e53a8de64bd095cc11a
GitHub Action <action@github.com>
2025-09-11 23:51:39 +0000

    chore: move Percentage of Users Attended a Contest to solutions/

--- solutions/Percentage of Users Attended a Contest/Percentage of Users Attended a Contest.sql
+++ solutions/Percentage of Users Attended a Contest/Percentage of Users Attended a Contest.sql
@@ -2,6 +2,6 @@
 SELECT r.contest_id, 
         ROUND(COUNT(r.user_id)*10.0 / (SELECT COUNT(*) FROM Users)*10.0, 2.0) AS percentage
 FROM Register r
-GROUP BY contest_id
-ORDER BY percentage DESC
+GROUP BY r.contest_id
+ORDER BY percentage DESC, r.contest_id ASC
 ;
