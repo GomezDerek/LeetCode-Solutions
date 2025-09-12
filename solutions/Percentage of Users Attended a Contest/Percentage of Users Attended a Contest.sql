73a5a6683c85af6ff41c32c0b94625e6049fae11
GitHub Action <action@github.com>
2025-09-12 00:02:41 +0000

    chore: move Percentage of Users Attended a Contest to solutions/

--- solutions/Percentage of Users Attended a Contest/Percentage of Users Attended a Contest.sql
+++ solutions/Percentage of Users Attended a Contest/Percentage of Users Attended a Contest.sql
@@ -1,6 +1,10 @@
 /* Write your T-SQL query statement below */
-SELECT r.contest_id, 
-        ROUND(COUNT(r.user_id)*10.0 / (SELECT COUNT(*) FROM Users)*10.0, 2.0) AS percentage
+WITH cte(total_users)
+AS
+(SELECT COUNT(*) FROM Users)
+
+SELECT r.contest_id
+        ,ROUND(COUNT(r.user_id)*10.0 / (SELECT total_users FROM cte)*10.0, 2.0) AS percentage
 FROM Register r
 GROUP BY r.contest_id
 ORDER BY percentage DESC, r.contest_id ASC
