9e9de634f964535c2cb350c98dcff40ccfa96b1b
GitHub Action <action@github.com>
2025-09-20 09:12:33 +0000

    chore: move Monthly Transactions I to solutions/

--- solutions/Monthly Transactions I/Monthly Transactions I.sql
+++ solutions/Monthly Transactions I/Monthly Transactions I.sql
@@ -15,9 +15,9 @@ then join with transactions table
 SELECT FORMAT(trans_date, 'yyyy-MM') AS month
         ,country
         ,COUNT(id) AS trans_count
-        ,SUM(CASE WHEN state = 'approved' THEN 1 END) AS approved_count
+        ,SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) AS approved_count
         ,SUM(amount) AS trans_total_amount
-        ,SUM(CASE WHEN state='approved' THEN amount END) AS approved_total_amount
+        ,SUM(CASE WHEN state='approved' THEN amount ELSE 0 END) AS approved_total_amount
 FROM Transactions
 GROUP BY FORMAT(trans_date, 'yyyy-MM'), country
 ORDER BY COUNT(id) DESC
