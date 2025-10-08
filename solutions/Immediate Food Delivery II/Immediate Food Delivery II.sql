1516e279f03a9cc71e4395b66384e87879149de0
GitHub Action <action@github.com>
2025-10-08 03:26:54 +0000

    chore: move Immediate Food Delivery II to solutions/

--- solutions/Immediate Food Delivery II/Immediate Food Delivery II.sql
+++ solutions/Immediate Food Delivery II/Immediate Food Delivery II.sql
@@ -24,6 +24,12 @@ AS
     WHERE rn = 1
 )
 
-SELECT ( ROUND(1.0 * COUNT(CASE WHEN order_date = customer_pref_delivery_date THEN 1 END) / COUNT(*), 2) * 100) AS immediate_percentage
+SELECT ( 
+    ROUND(
+        COUNT(CASE WHEN order_date = customer_pref_delivery_date THEN 1 END) * 1.0 
+        / COUNT(*) 
+        * 100
+    , 2)
+    ) AS immediate_percentage
 FROM first_orders
 ;
