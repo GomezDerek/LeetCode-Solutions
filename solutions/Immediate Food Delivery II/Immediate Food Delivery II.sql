4c50fb7447f45b1201d8e37c120b91dffdc746c7
GitHub Action <action@github.com>
2025-10-11 21:49:42 +0000

    chore: move Immediate Food Delivery II to solutions/

--- solutions/Immediate Food Delivery II/Immediate Food Delivery II.sql
+++ solutions/Immediate Food Delivery II/Immediate Food Delivery II.sql
@@ -14,7 +14,7 @@ SELECT
             WHEN order_date = customer_pref_delivery_date
             THEN 1.0
             ELSE 0.0
-        END) * 100.0
+        END) * 100
     ,2) 
     AS immediate_percentage
 FROM FirstOrders
