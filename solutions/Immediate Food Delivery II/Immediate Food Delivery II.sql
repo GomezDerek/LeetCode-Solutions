bf5469d895a763757d212155727023ed26d729b7
GitHub Action <action@github.com>
2025-10-11 21:23:48 +0000

    chore: move Immediate Food Delivery II to solutions/

--- solutions/Immediate Food Delivery II/Immediate Food Delivery II.sql
+++ solutions/Immediate Food Delivery II/Immediate Food Delivery II.sql
@@ -1,19 +1,22 @@
-WITH FirstOrders AS (
+/* Write your T-SQL query statement below */
+
+WITH first_orders AS
+(
     SELECT 
-        customer_id,
-        order_date,
-        customer_pref_delivery_date,
-        ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY order_date) AS rn
+        customer_id 
+        ,order_date
+        ,customer_pref_delivery_date
+        ,ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY order_date) AS rn
     FROM Delivery
 )
-SELECT 
+
+SELECT ( 
     ROUND(
-        AVG(CASE 
-            WHEN order_date = customer_pref_delivery_date 
-            THEN 1.0 
-            ELSE 0.0 
-        END) * 100, 
-        2
+        COUNT(CASE WHEN order_date = customer_pref_delivery_date THEN 1 END) * 1.0 
+        / COUNT(*) 
+        * 100
+    , 2)
     ) AS immediate_percentage
-FROM FirstOrders
-WHERE rn = 1;
+FROM first_orders
+WHERE rn = 1
+;
