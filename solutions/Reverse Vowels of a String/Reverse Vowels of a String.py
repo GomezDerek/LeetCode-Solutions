c2c1ddb8ea74a72510991115432a225748b8ee00
GitHub Action <action@github.com>
2025-10-12 00:17:41 +0000

    chore: move Reverse Vowels of a String to solutions/

--- solutions/Reverse Vowels of a String/Reverse Vowels of a String.py
+++ solutions/Reverse Vowels of a String/Reverse Vowels of a String.py
@@ -1,16 +1,16 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
-        vowels = {'a','e','i','o','u'} # set
+        vowels = {'a','e','i','o','u','A','E','I','O','U'} # set
         
         sArr = [ch for ch in s]
         l, r = [0, len(s)-1]
 
         while (l < r):
             # point both pointers to vowels
-            while ( l < r and sArr[l].lower() not in vowels):
+            while ( l < r and sArr[l] not in vowels):
                 l+=1
 
-            while ( l < r and sArr[r].lower() not in vowels ):
+            while ( l < r and sArr[r] not in vowels ):
                 r-=1
 
             # swap chars
