6a4c18dff5391d85d60608fd74fdbcbebc631b86
GitHub Action <action@github.com>
2025-10-16 00:26:33 +0000

    chore: move Product of Array Except Self to solutions/

--- solutions/Product of Array Except Self/Product of Array Except Self.py
+++ solutions/Product of Array Except Self/Product of Array Except Self.py
@@ -1,17 +1,24 @@
+# just watched NC's explanation for O(1) space solution
+# STRATEGY:
+#   because output doesn't contribute to extra memory
+#   use output array for prefix
+#   use a single var for suffix calcs 
+
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         N = len(nums)
-        prefix = [1]
+
+        result = [1] * N
+
+        # calc prefixes 1st
         for i in range(1, N):
-            prefix.append( prefix[i-1] * nums[i-1] )
+            result[i] = nums[i-1] * result[i-1]
 
-        suffix = [None] * N
-        suffix[-1] = 1
-        for i in range(N-2, -1, -1):
-            suffix[i] = suffix[i+1] * nums[i+1]
+        # then calc suffixes
+        suffix = 1
+        for i in range(N-1, -1, -1):
+            result[i] *= suffix
+            suffix *= nums[i]
 
-        ans = [None] * N
-        for i in range(N):
-            ans[i] = prefix[i] * suffix[i]
-
-        return ans
+        return result
+            
