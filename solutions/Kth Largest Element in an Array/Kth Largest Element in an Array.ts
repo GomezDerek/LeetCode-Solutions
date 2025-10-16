c24ad474870c401236a1d6e2877e03e46488e1aa
GitHub Action <action@github.com>
2025-10-16 16:42:24 +0000

    chore: move Kth Largest Element in an Array to solutions/

--- solutions/Kth Largest Element in an Array/Kth Largest Element in an Array.ts
+++ solutions/Kth Largest Element in an Array/Kth Largest Element in an Array.ts
@@ -1,22 +1,30 @@
 /**
-    GOAL: return kth highest val from an array without sorting
+    just watched NC's explanation and then I coded it in Python
+    now trying in TS
+
     STRATEGY:
-        iterate through nums
-        and maintain a k-sized heap to track kth element
-        top-k means heap root needs to be smallest -> min-heap
-        
+        because output doesn't count towards mem complexity
+        - use output to calc prefixes
+        - use a single var for suffixes
  */
 
-function findKthLargest(nums: number[], k: number): number {
-    const heap: MinHeap<number> = new MinHeap();
-    
-    for (const num of nums) {
-        if (heap.size() < k) heap.insert(num);
-        else {
-            heap.insert(num);
-            heap.extractRoot(); // pop
-        }
+function productExceptSelf(nums: number[]): number[] {
+    const N: number = nums.length;
+
+    const output: number[] = new Array();
+    output[0] = 1;
+
+    // calc prefixes 1st
+    for (let i=1; i<N; i++) {
+        output[i] = nums[i-1] * output[i-1];
     }
 
-    return heap.root();
+    // then calc suffixes
+    let suffix: number = 1;
+    for (let i=N-1; i>=0; i--) {
+        output[i] *= suffix;
+        suffix *= nums[i];
+    }
+
+    return output;
 };
