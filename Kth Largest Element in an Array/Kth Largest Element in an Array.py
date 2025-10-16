3c38307b9d50357d209b26c4579d128c997123ba
Derek Gomez <63622469+GomezDerek@users.noreply.github.com>
2025-10-15 21:51:18 -0700

    ADD Kth Largest Element in an Array

---
+++ Kth Largest Element in an Array/Kth Largest Element in an Array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heap = [] # min-heap by default
+
+        for num in nums:
+            if len(heap) < k: heapq.heappush(heap, num)
+            else: heapq.heappushpop(heap, num)
+
+        return heap[0]
