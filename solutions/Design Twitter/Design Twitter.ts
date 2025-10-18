a2d8d57cec9887fbbe577c9e4db0c2c97ff1049f
GitHub Action <action@github.com>
2025-10-18 08:24:52 +0000

    chore: move Design Twitter to solutions/

--- solutions/Design Twitter/Design Twitter.ts
+++ solutions/Design Twitter/Design Twitter.ts
@@ -1,15 +1,5 @@
-// because newsfeed is only self/friends tweets.
-// unfollows need to update newsfeed
-
-// to track followers
-// map = {userId: {...followees}}
-
-// newsfeed is deque for frontpush
-
-
-// to get user's newsfeed
-// newsfeed.filter(tweet => following[userId].has(tweet.user))
-
+// helper alias & class
+type UserId = number;
 class Tweet {
     userId: number;
     tweetId: number;
@@ -19,29 +9,21 @@ class Tweet {
     }
 }
 
-type UserId = number;
 
 class Twitter {
-    // use this DS for front insertion
-    // allTweets: Deque<number> = new Deque<number>();
-    // nvm
     allTweets: Tweet[] = [];
 
-    // O(1) ops
-    // following: Set<number> = new Set<number();
+    // {follower: Set([...followees])}
     following: {[key: UserId]: Set<UserId>} = {};
     
     constructor() {}
 
     postTweet(userId: number, tweetId: number): void {
-        // console.log('post');
-        const tweet: Tweet = new Tweet(userId, tweetId);
-        this.allTweets.push(tweet);
+        this.allTweets.push( new Tweet(userId, tweetId) );
     }
 
     getNewsFeed(userId: number): number[] {
         const followList: Set<UserId> = this.following[userId];
-        // console.log(`newsfeed: ${[...this.allTweets].map(tweet=>tweet.tweetId)} \nfollowList: ${followList}`);
         const newsFeed: number[] = [];
 
         // iterate backwards through all tweets to build newsfeed
@@ -49,10 +31,9 @@ class Twitter {
         while (newsFeed.length < 10 && i>=0) {
             const feedTweet: Tweet = this.allTweets[i];
 
-            // if followList is undefined, returns undefined / falsey
             if (
-                followList?.has(feedTweet.userId) 
-                || feedTweet.userId === userId
+                followList?.has(feedTweet.userId) // if user is following tweeter
+                || feedTweet.userId === userId    // OR it's a self-tweet
             ) {
                 newsFeed.push(feedTweet.tweetId);
             }
@@ -63,20 +44,17 @@ class Twitter {
     }
 
     follow(followerId: number, followeeId: number): void {
-        // console.log('follow');
+        // if following list exists, add followee
         if (this.following[followerId] !== undefined) {
             this.following[followerId].add(followeeId);
         }
+        // else following list DNE, so initialize
         else this.following[followerId] = new Set<UserId>([followeeId]);
     }
 
-    // ASSUME they are already following
-    // looks like my assumption is wrong...
+    // optional chaining because follower may not already be following followee
     unfollow(followerId: number, followeeId: number): void {
-        // console.log('unfollow');
-        // this.following[followerId].delete(followeeId);
         this.following[followerId]?.delete(followeeId);
-
     }
 }
 
