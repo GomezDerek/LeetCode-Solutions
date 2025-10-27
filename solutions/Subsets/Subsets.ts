5974ccad343776acc5a6326a1aee0cdeb0f9716d
GitHub Action <action@github.com>
2025-10-27 18:44:31 +0000

    chore: move Subsets to solutions/

--- solutions/Subsets/Subsets.ts
+++ solutions/Subsets/Subsets.ts
@@ -1,20 +1,3 @@
-/**
-    STRATEGY:
-        brute force
-        recurse through all possibilities
-        O(N!)
-
-
-    SIMULATION:
-    powerset = [ [], 
-                [1], 
-                [2], [1,2]
-                [3], [3,2], [3,1,2], [3,1]
-                [4], 4 + [3...], 4 + [2...], 4+ [1...]
-            ]
-
- */
-
 function subsets(nums: number[]): number[][] {
     const hash: Map<number, number[][]> = new Map<number, number[][]>();
 
@@ -32,19 +15,9 @@ function subsets(nums: number[]): number[][] {
         hash.set(num, subsets);
     }
 
-    console.log(hash);
-
-    // [ value1, value2, ... ]
-    // [ [[], [], []] , ... ]
-    // [] + []
-    // arr.join() -> string
-    // [].push(...[])
     const res: number[][] = [[]];
-    // hash.values().forEach( parentArray => {
     for (const parentArray of hash.values()) {
         parentArray.forEach(childArray => { res.push([...childArray]) })
     };
     return res;
-    // return [...hash.values()];
-    // return [[]];
 };
