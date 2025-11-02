97cbeb64fb0f180fdb74b29a474eb588ec7d55b4
Derek Gomez <63622469+GomezDerek@users.noreply.github.com>
2025-11-01 19:06:26 -0700

    ADD Longest Substring Without Repeating Characters

---
+++ Longest Substring Without Repeating Characters/Longest Substring Without Repeating Characters.ts
@@ -0,0 +1,34 @@
+/**
+STRATEGY:
+    sliding window
+    iterate with 2 pointers,
+        one for beginning 
+        one for end
+    use a set to track which chars are in the window
+    when a duplicate is found, 
+        iterate left pointer until duplicate is removed
+ */
+
+function lengthOfLongestSubstring(s: string): number {
+    let longest: number = 0;
+    const set: Set<string> = new Set<string>();
+
+    let [l,r] = [0,0];
+    while (r < s.length) {
+        // if non-valid window
+        if (set.has(s[r])) {
+            // move left pointer to duplicate, and remove from set as we go
+            while (s[l] !== s[r]) set.delete(s[l++]);
+            l++; // ++ one more time for duplicateIndex+1
+        }
+        // else valid window
+        else {
+            set.add(s[r]);
+            longest = Math.max(longest, r-l+1);
+        }
+
+        r++;
+    }
+
+    return longest;
+};
