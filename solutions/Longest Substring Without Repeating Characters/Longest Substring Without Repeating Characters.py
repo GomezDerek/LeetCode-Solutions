86d68b6a6eb90d833ea315020b1334dbc6d1de30
GitHub Action <action@github.com>
2025-11-02 02:34:56 +0000

    chore: move Longest Substring Without Repeating Characters to solutions/

--- solutions/Longest Substring Without Repeating Characters/Longest Substring Without Repeating Characters.py
+++ solutions/Longest Substring Without Repeating Characters/Longest Substring Without Repeating Characters.py
@@ -2,17 +2,16 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         res = 0
 
-        ch_set = set()
+        ch_map = {} # key: ch, val: s index for most recent ch
 
         l = 0
         for r, ch in enumerate(s):
-            if ch in ch_set:
-                while (s[l] != ch):
-                    ch_set.remove(s[l])
-                    l+=1
-                l+=1
+            if ch in ch_map and ch_map[ch] < r:
+                l = ch_map[ch] + 1
+                
             else:
-                ch_set.add(ch)
                 res = max(res, r-l+1)
 
+            ch_map[ch] = r
+            
         return res
