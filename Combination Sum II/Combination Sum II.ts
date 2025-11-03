31d39be53679f30ed670b61fb4699e07a47f7d46
Derek Gomez <63622469+GomezDerek@users.noreply.github.com>
2025-11-03 12:39:35 -0800

    ADD Combination Sum II

---
+++ Combination Sum II/Combination Sum II.ts
@@ -0,0 +1,32 @@
+// revised my code after watching NC's solution
+
+function combinationSum2(candidates: number[], target: number): number[][] {
+    const allCombos: number[][] = [];
+    const curCombo: number[] = [];
+    candidates.sort();
+    dfs(0, 0);
+    return allCombos;
+
+    function dfs(i: number, curSum: number): void {
+        // good base case (target found)
+        if (curSum === target) {
+            allCombos.push([...curCombo]);
+            return;
+        }
+
+        // bad base case (out of bounds or target exceeded)
+        else if (curSum > target || i >= candidates.length) {
+            return;
+        }
+
+        // recursion
+        // include cur candidate
+        curCombo.push(candidates[i]);
+        dfs(i+1, curSum+candidates[i]);
+
+        // exclude cur candidate
+        curCombo.pop();
+        while ( candidates[i] === candidates[i+1]) i++;
+        dfs(i+1, curSum);
+    }
+};
