872647fcc9ecdc3e9d712efa9f13d756ee17d0b3
GitHub Action <action@github.com>
2025-11-20 18:43:01 +0000

    chore: move Generate Parentheses to solutions/

--- solutions/Generate Parentheses/Generate Parentheses.ts
+++ solutions/Generate Parentheses/Generate Parentheses.ts
@@ -1,34 +1,3 @@
-/**
-STRATEGY:
-    recursive dfs backtracking
-    decision tree: ( )  O(2^n)
-
-    open_counter = 2
-    closed_counter = 2
-           ""
-        "("      1
-    "(("2  "()"0
-           "()(" 1
-    
-
-    allCombos = str[]
-    dfs(curCombo: str):
-        // good base case
-        pairs of parantheses == n: push to allcombos
-
-        // bad base case
-        curCmbo not valid parentheses
-        bc closed_counter > open_counter
-
-
-        recurse and add open parenth.
-        // pop and update counters between recursions
-        recurse and closed parenth.
-        // pop and update counters between recursions
-
-
- */
-
 function generateParenthesis(n: number): string[] {
     const allCombos: string[] = [];
     let open_count: number = 0;
@@ -45,32 +14,25 @@ function generateParenthesis(n: number): string[] {
 
         // good base case
         else if (closed_count === n) {
-            // allCombos.push(curCombo.slice());  
             allCombos.push(curCombo.join(""));  
             return;
         }
 
         // open parenth recursion
-        // curCombo.concat("(");
-        // curCombo += "(";
         curCombo.push("(");
         open_count++;
         dfs();
 
         // popping the open parenth
-        // curCombo = curCombo.slice(0,curCombo.length-1);
         curCombo.pop();
         open_count--;
 
         // closed parenth recursion
-        // curCombo.concat(")");
-        // curCombo += ")";
         curCombo.push(")");
         closed_count++;
         dfs();
 
         // pop the close parenth
-        // curCombo = curCombo.slice(0,curCombo.length-1);
         curCombo.pop();
         closed_count--;
     }
