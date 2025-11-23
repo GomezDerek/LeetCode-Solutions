395d064ff32b469f06a42a07b0f1827f2da167eb
GitHub Action <action@github.com>
2025-11-23 23:11:56 +0000

    chore: move Container With Most Water to solutions/

--- solutions/Container With Most Water/Container With Most Water.ts
+++ solutions/Container With Most Water/Container With Most Water.ts
@@ -7,10 +7,8 @@ STRATEGY:
     optimized: 2 pointer / sliding window
 
  */
-
-// attempt after reading hint 2: "Try to use two-pointers. Set one pointer to the left and one to the right of the array. Always move the pointer that points to the lower line."
-
-function maxArea(height: number[]): number {
+ 
+ function maxArea(height: number[]): number {
     let maxArea: number = 0;
     let l: number = 0;
     let r: number = height.length -1;
@@ -21,28 +19,8 @@ function maxArea(height: number[]): number {
         );
 
         if (height[l] < height[r]) l++;
-        // else height[l] > height[r]
         else r--;
     }
 
     return maxArea;   
 }
-
-// 1st 2-pointer attempt
-// function maxArea(height: number[]): number {
-//     let maxArea: number = 0;
-//     let l: number = 0;
-//     let r: number = 1;
-
-//     while (r < height.length) {
-//         maxArea = Math.max(maxArea,
-//             (r-l) * Math.min(height[l], height[r])
-//         );
-
-//         if (height[r] > height[l]) l = r;
-
-//         r++;
-//     }
-
-//     return maxArea;
-// };
