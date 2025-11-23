a6338271f23fc822887e215408a128db73f028e0
GitHub Action <action@github.com>
2025-11-23 23:05:54 +0000

    chore: move Container With Most Water to solutions/

--- solutions/Container With Most Water/Container With Most Water.ts
+++ solutions/Container With Most Water/Container With Most Water.ts
@@ -2,22 +2,47 @@
 GOAL: find the largest 2D area of water
 
 STRATEGY:
-    calc all areas and track the largest O(n^2) create all left/right combos
+    brute force: calc all areas and track the largest O(n^2) create all left/right combos
 
     optimized: 2 pointer / sliding window
 
  */
 
+// attempt after reading hint 2: "Try to use two-pointers. Set one pointer to the left and one to the right of the array. Always move the pointer that points to the lower line."
+
 function maxArea(height: number[]): number {
     let maxArea: number = 0;
+    let l: number = 0;
+    let r: number = height.length -1;
 
-    for (let l=0; l<height.length; l++) {
-        for (let r=1; r<height.length; r++) {
-            maxArea = Math.max(maxArea, 
-                (r-l) * Math.min(height[l], height[r])
-            );
-        }
+    while (l < r) {
+        maxArea = Math.max(maxArea,
+            (r-l) * Math.min(height[l], height[r])
+        );
+
+        if (height[l] < height[r]) l++;
+        // else height[l] > height[r]
+        else r--;
     }
 
-    return maxArea;
-};
+    return maxArea;   
+}
+
+// 1st 2-pointer attempt
+// function maxArea(height: number[]): number {
+//     let maxArea: number = 0;
+//     let l: number = 0;
+//     let r: number = 1;
+
+//     while (r < height.length) {
+//         maxArea = Math.max(maxArea,
+//             (r-l) * Math.min(height[l], height[r])
+//         );
+
+//         if (height[r] > height[l]) l = r;
+
+//         r++;
+//     }
+
+//     return maxArea;
+// };
