53bcb684d7d5c0614f901a924f6c79fe4828f685
GitHub Action <action@github.com>
2025-12-11 05:15:57 +0000

    chore: move Palindrome Partitioning to solutions/

--- solutions/Palindrome Partitioning/Palindrome Partitioning.ts
+++ solutions/Palindrome Partitioning/Palindrome Partitioning.ts
@@ -1,61 +1,44 @@
 function partition(s: string): string[][] {
     const allPartitions: string[][] = [];
     const curPartition: string[] = [];
-    recurse(0);
+    dfs(0);
     return allPartitions;
-    
-    // func defs
-    function recurse(i: number): void {
+
+    function dfs(i: number): void {
         // base case
-        if (i >= s.length) {
+        if (i === s.length) {
             allPartitions.push([...curPartition]);
             return;
         }
-        
+
         // ops
         const palindromes: string[] = [];
         for (let j=i; j<s.length; j++) {
-            // generate all substrings from i to end
-            // create array of palindromic substrs
-            const substr: string = s.slice(i,j+1);
+            const substr: string = s.slice(i, j+1);
             if (isPalindrome(substr)) palindromes.push(substr);
         }
 
         // recursion
         for (const palindrome of palindromes) {
             curPartition.push(palindrome);
-            recurse(i+palindrome.length);    
+            dfs(i+palindrome.length);
             curPartition.pop();
         }
-
-    }
-
-    // helper function
-    function isPalindrome(str: string): boolean {
-        let i: number;
-        let j: number;
-
-        // if length is even
-        if (str.length % 2 === 0) {
-            j = str.length/2;
-            i = j - 1;
-        }
-
-        // if length is odd
-        else {
-            i = Math.floor(str.length / 2);
-            j = i;
-        }
-
-        // validate palindrome
-        while ( i>=0 && j<str.length ) {
-            if (str[i] !== str[j]) return false; // not valid
-            else {
-                i--;
-                j++;
-            }
-        }
-        // validated
-        return true;
     }
 };
+
+// helper - palindrome checker
+function isPalindrome(s: string): boolean {
+    let i: number = 0
+    let j: number = s.length-1;
+
+    // find invalidity
+    while (i <= j) {
+        if (s[i] !== s[j]) return false;
+        i++;
+        j--;
+    }
+
+    // passed all validity checks
+    return true;
+}
