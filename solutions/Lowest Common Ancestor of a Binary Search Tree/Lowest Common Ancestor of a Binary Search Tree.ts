349ebb125c9cadcbd748f2daabf32ee0280817d6
GitHub Action <action@github.com>
2025-12-15 07:12:50 +0000

    chore: move Lowest Common Ancestor of a Binary Search Tree to solutions/

--- solutions/Lowest Common Ancestor of a Binary Search Tree/Lowest Common Ancestor of a Binary Search Tree.ts
+++ solutions/Lowest Common Ancestor of a Binary Search Tree/Lowest Common Ancestor of a Binary Search Tree.ts
@@ -12,50 +12,23 @@
  * }
  */
 
- /**
-GOAL: in a BST, find the closest parent/ancestor of 2 nodes
-
-STRATEGY:
-    for each target node,
-        - create an ancestry list, from root to parent
-    
-    simultaneously iterate through both ancestry lists,
-    until ancestors diverge
-
-NOTES:
-    - nodes can be their own parents
-    
-  */
+/**
+    STRATEGY:
+        travel through the BST, progressing towards both p and q nodes
+        once they must diverge OR one of the nodes has been found, 
+            we have found our LCA
+*/
 
 function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
-    const path1: TreeNode[] = [];
-    const path2: TreeNode[] = [];
+    // base case
+    console.log(root.val);
+    if (root === p) return root;
+    else if (root === q) return root;
 
-    createPath(root, path1, p);
-    createPath(root, path2, q);
-
-    let lca: TreeNode = root;
-    let i: number = 0;
-    while(i < Math.min(path1.length, path2.length) && path1[i].val === path2[i].val) {
-        lca = path1[i];
-        i++;
-    }
-    return lca;
-
-    // func def
-    function createPath(curNode: TreeNode | null, path: TreeNode[], targetNode: TreeNode): void {
-        // base case
-        if (curNode === null) return;
-        else if (curNode.val === targetNode.val) {
-            path.push(curNode);
-            return;
-        }
-
-        // ops
-        path.push(curNode);
-
-        // recursion
-        if (curNode.val < targetNode.val) createPath(curNode.right, path, targetNode);
-        else if (curNode.val > targetNode.val) createPath(curNode.left, path, targetNode);
-    }
+    // recursion
+    else if (p.val > root.val && q.val > root.val) return lowestCommonAncestor(root.right, p, q);
+    else if (p.val < root.val && q.val < root.val) return lowestCommonAncestor(root.left, p, q);
+    
+    // paths diverge
+    else return root; 
 };
