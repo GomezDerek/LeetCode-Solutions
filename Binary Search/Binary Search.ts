51be44d232724263a4e9fa055ae4ed1cb1f45962
Derek Gomez <63622469+GomezDerek@users.noreply.github.com>
2025-12-17 10:12:26 -0800

    ADD Binary Search

---
+++ Binary Search/Binary Search.ts
@@ -0,0 +1,13 @@
+function search(nums: number[], target: number): number {
+    let [l,r] = [0, nums.length]
+    
+    let m: number
+    while (l <= r) {
+        m = Math.floor( (l+r)/2 )
+        if (nums[m] === target) return m
+        else if (nums[m] > target) r = m-1
+        else if (nums[m] < target) l = m+1
+    }
+
+    return -1 // target DNE
+};
