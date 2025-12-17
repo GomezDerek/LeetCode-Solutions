c8e2f89ba7deca5afc36c5fe5c50eb1d72375851
Derek Gomez <63622469+GomezDerek@users.noreply.github.com>
2025-12-17 11:54:47 -0800

    ADD Number of Islands

---
+++ Number of Islands/Number of Islands.ts
@@ -0,0 +1,42 @@
+// Breadth First Search approach
+function numIslands(grid: string[][]): number {
+    const [M,N]: [number, number] = [grid.length, grid[0].length];
+    let numIslands: number = 0;
+
+    for (let i=0; i<M; i++) {
+        for (let j=0; j<N; j++) {
+            if (grid[i][j] === '1') {
+                numIslands++;
+                bfs(i,j);
+            }
+        }
+    }
+
+    return numIslands;
+
+    //////////////////
+    // BFS func dec //
+    //////////////////
+    function bfs(x: number, y: number): void {
+        const dq: Deque<[number, number]> = new Deque<[number, number]>();
+        dq.pushBack([x,y]);
+
+        while (!dq.isEmpty()) {
+            const [cx,cy] = dq.popFront();
+            
+            // base cases
+            // out of bounds
+            if (cx < 0 || cx >= M || cy < 0 || cy >= N) continue;
+
+            // alr visited
+            else if (grid[cx][cy] === '0') continue;
+
+            // else mark as visited and traverse to neighbors
+            grid[cx][cy] = '0';
+            dq.pushBack([cx+1,cy]);
+            dq.pushBack([cx-1,cy]);
+            dq.pushBack([cx,cy+1]);
+            dq.pushBack([cx,cy-1]);
+        }
+    }
+};
