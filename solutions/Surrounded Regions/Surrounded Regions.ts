f0f6ab2bd62b20014e05c1ce481121e069192043
GitHub Action <action@github.com>
2025-12-29 03:31:29 +0000

    chore: move Surrounded Regions to solutions/

--- solutions/Surrounded Regions/Surrounded Regions.ts
+++ solutions/Surrounded Regions/Surrounded Regions.ts
@@ -23,21 +23,29 @@ function solve(board: string[][]): void {
 
     // store coordinates as string literals
     const unsurrounded = new Set<string>();
+    const allRegions = new Set<string>();
 
     const q: [number, number][] = [];
 
-    // add all border O's to queue
-    for (let col=0; col<m; col++) {
-        if (board[0][col] === 'O') q.push([0,col]);     // top border
-        if (board[n-1][col] === 'O') q.push([n-1,col]); // bottom border
-    }
+    // process all O's
     for (let row=0; row<n; row++) {
-        if (board[row][0] === 'O') q.push([row,0]);      // left border
-        if (board[row][m-1] === 'O') q.push([row,m-1]);  // right border
+        for (let col=0; col<m; col++) {
+            if (board[row][col] === 'X') continue;
+
+            // if O on border, add to unsurrounded
+            else if (row === 0 || row === n-1 || col === 0 || col === m-1) {
+                unsurrounded.add(`${row} ${col}`);
+                q.push([row,col]);
+            }
+            
+            // else add O to allRegions
+            else {
+                allRegions.add(`${row} ${col}`);
+            }
+        }
     }
 
     // execute BFS - add to set when we add to queue
-    for (const [x,y] of q) unsurrounded.add(`${x} ${y}`);
     let head = 0;
     while (head < q.length) {
         const [x,y] = q[head++];
@@ -45,13 +53,8 @@ function solve(board: string[][]): void {
         // traverse to neighbors
         for (const [nx,ny] of [[x+1,y],[x-1,y],[x,y+1],[x,y-1]]) {
             if (
-                // in bounds
-                nx >= 0 &&
-                nx < n &&
-                ny >= 0 &&
-                ny < m &&
                 // is an unvisited region
-                board[nx][ny] === 'O' &&
+                allRegions.has(`${nx} ${ny}`) &&
                 !unsurrounded.has(`${nx} ${ny}`)
             ) {
                 unsurrounded.add(`${nx} ${ny}`);
@@ -60,13 +63,11 @@ function solve(board: string[][]): void {
         }
     }
 
-    // now that all unsurrounded regions are in our set,
-    // we can now capture all surrounded regions
-    for (let row=1; row<n-1; row++) {
-        for (let col=1; col<m-1; col++) {
-            if (board[row][col] === 'O' && !unsurrounded.has(`${row} ${col}`)) {
-                board[row][col] = 'X';
-            }
+    // capture all surrounded regions
+    for (const region of allRegions) {
+        if (!unsurrounded.has(region)) {
+            const [x,y] = region.split(" ");
+            board[x][y] = 'X';
         }
     }
 };
