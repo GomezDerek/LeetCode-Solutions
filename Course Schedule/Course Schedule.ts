b6c16786737858a1f843d08751041080ad6ef159
Derek Gomez <63622469+GomezDerek@users.noreply.github.com>
2025-12-30 18:53:35 -0800

    ADD Course Schedule

---
+++ Course Schedule/Course Schedule.ts
@@ -0,0 +1,33 @@
+function canFinish(numCourses: number, prerequisites: number[][]): boolean {
+    const prereqs: number[][] = new Array(numCourses);
+    for (let i=0; i<numCourses; i++) prereqs[i] = [];
+
+    for (let i=0; i<prerequisites.length; i++) {
+        const [course, prereq] = prerequisites[i];
+        prereqs[course].push(prereq);
+    }
+
+    const visited = new Set<number>();
+    for (let course=0; course<numCourses; course++) {
+        if (hasCycle(course)) return false;
+    }
+
+    // all courses are cycle free!
+    return true;
+
+    function hasCycle(course: number): boolean {
+        if (visited.has(course)) return true; // cycle detected
+
+        visited.add(course);
+
+        for (let i=0; i<prereqs[course].length; i++) {
+            const prereq = prereqs[course][i];
+            if (hasCycle(prereq)) return true; // cycle detected in prereq
+        }
+
+        visited.delete(course);
+        prereqs[course] = [];
+
+        return false; // course has no cycle
+    }
+};
