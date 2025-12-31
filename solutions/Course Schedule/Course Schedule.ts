0515a942f2786aa8994aa5f877a6a637eb7097d1
GitHub Action <action@github.com>
2025-12-31 02:46:01 +0000

    chore: move Course Schedule to solutions/

--- solutions/Course Schedule/Course Schedule.ts
+++ solutions/Course Schedule/Course Schedule.ts
@@ -1,17 +1,15 @@
 function canFinish(numCourses: number, prerequisites: number[][]): boolean {
     const prereqs: number[][] = new Array(numCourses);
-    for (let i=0; i<numCourses; i++) {
-        prereqs[i] = [];
-    }
+    for (let i=0; i<numCourses; i++) prereqs[i] = [];
 
-    for (const [course, prereq] of prerequisites) {
+    for (let i=0; i<prerequisites.length; i++) {
+        const [course, prereq] = prerequisites[i];
         prereqs[course].push(prereq);
     }
 
     const visited = new Set<number>();
     for (let course=0; course<numCourses; course++) {
         if (hasCycle(course)) return false;
-        prereqs[course] = [];
     }
 
     // all courses are cycle free!
@@ -26,10 +24,10 @@ function canFinish(numCourses: number, prerequisites: number[][]): boolean {
         for (let i=0; i<prereqs[course].length; i++) {
             prereq = prereqs[course][i];
             if (hasCycle(prereq)) return true; // cycle detected in prereq
-            else prereqs[prereq] = [];
         }
 
         visited.delete(course);
+        prereqs[course] = [];
 
         return false; // course has no cycle
     }
