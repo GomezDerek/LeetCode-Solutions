6f4ed45ad048204a09668c047393e5d615c6b7c9
GitHub Action <action@github.com>
2025-12-31 01:56:31 +0000

    chore: move Course Schedule to solutions/

--- solutions/Course Schedule/Course Schedule.ts
+++ solutions/Course Schedule/Course Schedule.ts
@@ -1,28 +1,9 @@
-/**
-GOAL: determine if this graph has a cycle
-STRAT:
-    - create dict of classes and their prereqs
-    - for each class, traverse graph and look for cycles
-    - if cycle found, return false
-    - else continue traversing each class in dict
-    - once all classes have been checked, return true
-
-    - for each class's prereq traversal, keep a visited set
-    - optimization: when a class has been verified, 
-                    remove prereqs to prevent retraversal when checking other classse
-
-NOTES:
-    at least 1 course guaranteed
-    up to 5k edges
-
-done planning at 9min
- */
-
 function canFinish(numCourses: number, prerequisites: number[][]): boolean {
     const prereqs: number[][] = new Array(numCourses);
     for (let i=0; i<numCourses; i++) {
         prereqs[i] = [];
     }
+
     for (const [course, prereq] of prerequisites) {
         prereqs[course].push(prereq);
     }
@@ -30,29 +11,25 @@ function canFinish(numCourses: number, prerequisites: number[][]): boolean {
     const visited = new Set<number>();
     for (let course=0; course<numCourses; course++) {
         if (hasCycle(course)) return false;
+        prereqs[course] = [];
     }
 
     // all courses are cycle free!
     return true;
 
-    // return true if class has cycle, false if no cycle
     function hasCycle(course: number): boolean {
-        // base case
         if (visited.has(course)) return true; // cycle detected
 
-        // pre-recurse op
         visited.add(course);
 
-        // recurse
-        for (const prereq of prereqs[course]) {
-            if (hasCycle(prereq)) return true; // cycle in prereq
-            else prereqs[course] = []; // no cycle in prereq, clear to avoid retraversal
+        // for (const prereq of prereqs[course]) {
+        for (let i=0; i<prereqs[course].length; i++) {
+            if (hasCycle( prereqs[course][i] )) return true; // cycle detected in prereq
+            else prereqs[course] = [];
         }
 
-        // post-recurse op
         visited.delete(course);
 
-        // return -> course has no cycle
-        return false;
+        return false; // course has no cycle
     }
 };
