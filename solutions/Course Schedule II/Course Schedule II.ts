9048a901631761e919560e9432ef5dfba478adb9
GitHub Action <action@github.com>
2026-01-05 03:39:05 +0000

    chore: move Course Schedule II to solutions/

--- solutions/Course Schedule II/Course Schedule II.ts
+++ solutions/Course Schedule II/Course Schedule II.ts
@@ -1,40 +1,62 @@
-// NC DFS solution
-
-function findOrder(numCourses: number, prerequisites: number[][]): number[] {
-    const adjList: number[][] = new Array(numCourses).fill(null).map(()=>[]);
-
-    // fill the adj list
-    for (const [course, prereq] of prerequisites) {
-        adjList[course].push(prereq);
-    }
-
-    const topoSort: number[] = [];
-
-    // run dfs on each course
-    const visited = new Set<number>();
-    const inCycle = new Set<number>();
-    for (let i=0; i<numCourses; i++) {
-        if ( !dfs(i) ) return [];
-    }
+/**
+GOAL: 
+    return a topological sort of courses
+STRAT:
+    ASSUME courses form a DAG, scratch that - cycles are possible
+    courses with no prereqs go first, courses with prereqs go last
     
-    return topoSort; // replace me
+    use topo sort output: set() because keeps insertion order
 
-    // returns if graph is non-cyclic
-    function dfs(curCourse: number): boolean {
-        
-        if (visited.has(curCourse)) return true;
-        else if (inCycle.has(curCourse)) return false;
+    1. create a map where key: prereq, val: courses to take next
+    2. iterate through courses:
+        a. add to topo sort output 
+        b. if prereq has next_courses:
+            i. nav to next_courses, repeat
 
-        inCycle.add(curCourse);
+
+NOTES:
+    no tricks or edge cases in input
+ */
+function findOrder(numCourses: number, prerequisites: number[][]): number[] {
+    const output = new Set<number>();
+
+    const nextCourses: number[][] = Array.from( {length: numCourses}, a => [] );
+    
+    for (const [course, prereq] of prerequisites) {
+        // nextCourses[prereq].push(course);
+        nextCourses[course].push(prereq);
+    }
+    console.log(nextCourses);
+
+    let cycleDetected = false;
+    const visited = new Set<number>();
+
+    for (let i=0; i<numCourses; i++) {
+        dfs(i);
+        if (cycleDetected) return []; // "If it is impossible to finish all courses, return an empty array.""
+        visited.clear();
+    }
+
+    return [...output];
+
+    // funcs hoisted
+    function dfs(i: number): void {
+        // base case(s)
+        if (visited.has(i)) {
+            cycleDetected = true;
+            return;
+        }
         
-        for (const prereq of adjList[curCourse]) {
-            if (!dfs(prereq)) return false;
+        // pre-ops
+        visited.add(i);
+        output.add(i);
+
+        // recurse
+        for (const nextCourse of nextCourses[i]) {
+            dfs(nextCourse);
         }
 
-        inCycle.delete(curCourse);
-        visited.add(curCourse);
-        topoSort.push(curCourse);
-
-        return true; 
+        // post-ops
+        visited.delete(i);
     }
 };
