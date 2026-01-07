720bc024a78fc3affcbe4aa3d7c5b467ba1e4e97
Derek Gomez <63622469+GomezDerek@users.noreply.github.com>
2026-01-07 11:52:28 -0800

    ADD Number of Connected Components in an Undirected Graph

---
+++ Number of Connected Components in an Undirected Graph/Number of Connected Components in an Undirected Graph.ts
@@ -0,0 +1,51 @@
+/**
+GOAL: count disconnected components
+STRAT:
+    - create the adj list
+    - dfs to traverse components
+        - as we visit nodes, empty neighbors in adj list to avoid retraversal
+    - to visit next component find an untraversed node with neighbors
+
+NOTES:
+    - at least 1 node & 1 edge
+    - no self-loops
+    - cycles are possible
+    - 0 indexed
+
+done planning at 5min
+ */
+function countComponents(n: number, edges: number[][]): number {
+    let numComponents: number = 0;
+    
+    const adjList: number[][] = Array.from({length: n}, ()=> []);
+    for (let i=0; i<edges.length; i++) {
+        const [a,b] = edges[i];
+        adjList[a].push(b);
+        adjList[b].push(a);
+    }
+
+    const visited = new Set<number>();
+    for (let i=0; i<n; i++) {
+        if (!visited.has(i)) {
+            dfs(i,-1);
+            numComponents++;
+        }
+    }
+
+    return numComponents;
+
+    // hoisted func
+    function dfs(node: number, prevNode: number): void {
+        // base case(s)
+        if (visited.has(node)) return;
+
+        // pre-recurse ops
+        visited.add(node);
+
+        // recurse
+        for (let i=0; i<adjList[node].length; i++) {
+            const neighbor = adjList[node][i];
+            if (neighbor !== prevNode) dfs(neighbor, node);
+        }
+    }
+};
